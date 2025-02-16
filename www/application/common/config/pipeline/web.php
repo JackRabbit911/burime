66d45b841db94151e72ace79111172a76a3a8d48
alx <world.zay@gmail.com>
2025-02-16 17:23:18 +0300

    Created views Alert and formmacro

--- www/application/common/config/pipeline/web.php
+++ www/application/common/config/pipeline/web.php
@@ -5,5 +5,5 @@ use Az\Session\SessionMiddleware;
 use Sys\I18n\I18nMiddleware;
 
 $this->pipe(I18nMiddleware::class);
-// $this->pipe(SessionMiddleware::class);
+$this->pipe(SessionMiddleware::class);
 // $this->pipe(AuthMiddleware::class);
