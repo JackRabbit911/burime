683f3a98b87f85a8e8e8cf98b5bae139a93a3143
alx <world.zay@gmail.com>
2025-02-20 17:11:00 +0300

    Updated controller Profile and some views

--- www/application/auth/Controller/Profile.php
+++ www/application/auth/Controller/Profile.php
@@ -5,10 +5,7 @@ namespace Auth\Controller;
 use Auth\Component\ProfileForm;
 use Auth\Middleware\ProfileValidation;
 use Auth\Model\AvatarRepo;
-use Auth\User;
-use Sys\Image\Im;
 use Sys\Template\ComponentForm;
-use Sys\Helper\Facade\File;
 use Az\Route\Route;
 use HttpSoft\Response\RedirectResponse;
 
@@ -29,50 +26,9 @@ class Profile extends AuthAbstract
     public function save(AvatarRepo $repo)
     {
         $this->user->update($this->request->getParsedBody())->save();
-
         $uploadedFile = $this->request->getUploadedFiles()['avatar'];
-
-        // dd($uploadedFile->getStream()->getContents());
-
         $repo->save($uploadedFile, $this->user->id);
-        // $this->saveAvatar();
 
         return new RedirectResponse($this->getReferer());
     }
-
-    // private function saveAvatar()
-    // {
-    //     $file = $this->request->getUploadedFiles()['avatar'];
-
-    //     if ($file->getError() !== UPLOAD_ERR_OK) {
-    //         return;
-    //     }
-
-    //     $config = config('user');
-
-    //     if (!is_dir($config['avatar_path'])) {
-    //         mkdir($config['avatar_path'], 0777);
-    //     }
-
-    //     if (!is_writable($config['avatar_path'])) {
-    //         chmod($config['avatar_path'], 0777);
-    //     }
-
-    //     $avatarPath = $config['avatar_path'] . $this->user->id;
-
-    //     foreach (glob($avatarPath . '.*') as $fn) {
-    //         unlink($fn);
-    //     }
-
-    //     $mime = $file->getClientMediaType();
-    //     $ext = File::extByImageType($mime);
-
-    //     $avatarPath .= $ext;
-    //     $file->moveTo($avatarPath);
-
-    //     chmod($avatarPath, 0666);
-        
-    //     Im::create($avatarPath)
-    //         ->thumb($config['avatar_size'])->save();
-    // }
 }
