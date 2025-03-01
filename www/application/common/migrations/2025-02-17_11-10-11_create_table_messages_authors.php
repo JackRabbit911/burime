a5b6bf24ba70580be5084a783e50af59f024879c
alx <world.zay@gmail.com>
2025-03-01 16:06:07 +0300

    Updated some migrations

--- www/application/common/migrations/2025-02-17_11-10-11_create_table_messages_authors.php
+++ www/application/common/migrations/2025-02-17_11-10-11_create_table_messages_authors.php
@@ -7,14 +7,11 @@ class _2025_02_17_11_10_11_create_table_messages_authors
         return "CREATE TABLE `messages_authors` (
         `message_id` int(10) unsigned NOT NULL,
         `author_id` int(10) unsigned NOT NULL,
-        `user_id` int(10) unsigned NOT NULL,
         `status` tinyint(3) unsigned NOT NULL,
         KEY `message_id` (`message_id`),
         KEY `author_id` (`author_id`),
-        KEY `user_id` (`user_id`),
         CONSTRAINT `messages_authors_ibfk_1` FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`),
-        CONSTRAINT `messages_authors_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
-        CONSTRAINT `messages_authors_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+        CONSTRAINT `messages_authors_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin";
     }
 
