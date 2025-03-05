11c02d2eb589f8b06d0b79971e7a5b799b126153
alx <world.zay@gmail.com>
2025-03-05 11:51:03 +0300

    Updated stringTrim.js

--- htdocs/www/assets/js/stringTrim.js
+++ htdocs/www/assets/js/stringTrim.js
@@ -3,12 +3,15 @@ document.addEventListener("DOMContentLoaded", stringTrim);
 function stringTrim() {
     document.querySelectorAll('[trim-line]').forEach(
         (node) => {
-            limit = Number(node.getAttribute('trim-line'))
-            const text = node.textContent.trim()
-            node.textContent = text.length < limit
-                ? text
-                : text.slice(0, limit).trim()
-                    + String.fromCharCode(8230)
+            limit = parseInt(node.getAttribute('trim-line'))
+
+            if (!isNaN(limit)) {
+                const text = node.textContent.trim()
+                node.textContent = text.length < limit
+                    ? text
+                    : text.slice(0, limit).trim()
+                        + String.fromCharCode(8230)
+            }
         }
     )
 }
