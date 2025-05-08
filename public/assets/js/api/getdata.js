837d609a7149a46e9d032a34a71ddcc7ef1cd775
alx <world.zay@gmail.com>
2025-05-08 17:44:15 +0300

    feat: Create Axios layer

--- public/assets/js/api/getdata.js
+++ public/assets/js/api/getdata.js
@@ -1,31 +1,15 @@
-const host = 'http://127.0.0.1:5500/site.zone/www/public/api/'
+const getConfigExtension = (tokenType) => ({
+    headers: {
+        'Authorization': tokenType + ' ' + sessionStorage.getItem(tokenType)
+    }
+})
 
 async function getData(url, tokenType = 'Bearer') {
-    const token = sessionStorage.getItem(tokenType)
-
-    if (!token) {
-        window.location.assign(host + 'login.html')
-    }
-
-    const response = await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': tokenType + ' ' + token
-        }
-    })
-
-    if (response.status == 401) {
-        window.location.assign(host + 'login.html')
-    }
-
-    if (response.headers.has('Bearer')) {
-        sessionStorage.setItem('Bearer', response.headers.get('Bearer'))
-    }
+    const response = await api.get(url, getConfigExtension(tokenType))
 
     if (response.headers.has('X-Token') && response.headers.get('X-Token') == 'Refresh') {
         return getData(url, 'Refresh')
     } else {
-        const json = await response.json()
-        return json
+        return response.data
     }
 }
