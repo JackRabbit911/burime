0aed7fe67c968dbf1cdc9d2ab4fb19a03eac4c00
alx <world.zay@gmail.com>
2025-06-20 15:12:54 +0300

    [feat] Create Api\Model\ModelRefreshToken class

---
+++ application/auth/Api/Model/ModelRefreshToken.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auth\Api\Model;
+
+use stdClass;
+use Sys\Model\MysqlModel;
+
+class ModelRefreshToken extends MysqlModel
+{
+    private string $table = 'refresh_tokens';
+
+    public function create(array $data)
+    {
+        $data['is_api'] = 1;
+        $this->qb->table($this->table)->insert($data);
+    }
+
+    public function read(string $token, string $user_agent, int $lifetime): stdClass|false
+    {
+        $expire = date('Y-m-d h:i:s', time() - $lifetime);
+
+        return $this->qb->table($this->table)
+            ->select('token', 'user_id', 'updated')
+            ->where('user_agent', '=', $user_agent)
+            ->where('is_api', '=', 1)
+            ->where('updated', '>', $expire)
+            ->find($token, 'token');
+    }
+
+    public function update(string $token, string $user_agent, array $data): void
+    {
+        $this->qb->table($this->table)
+            ->where('token', '=', $token)
+            ->where('user_agent', '=', $user_agent)
+            ->where('is_api', '=', 1)
+            ->update($data);
+    }
+
+    public function delete(string $token, string $user_agent): void
+    {
+        $this->qb->table($this->table)
+            ->where('token', '=', $token)
+            ->where('user_agent', '=', $user_agent)
+            ->where('is_api', '=', 1)
+            ->delete();
+    }
+}
