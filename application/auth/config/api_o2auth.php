4e13abc3b9f45bc9f313691ed0ba6555573471c5
alx <world.zay@gmail.com>
2025-06-21 12:03:12 +0300

    [feat] Update api_o2auth.php config

--- application/auth/config/api_o2auth.php
+++ application/auth/config/api_o2auth.php
@@ -3,6 +3,13 @@
 return [
     'algo' => 'HS256',
     'key' => env('OAUTH_KEY'),
-    'lifetime' => 30,
+    'lifetime' => 300,
     'iss' => env('APP_NAME'),
+    'refresh_lifetime' => 3600 * 2,
+    'refresh_update' => 3600,
+    'exclude_urls' => [
+        '/auth/login',
+        '/auth/logout',
+        '/console'
+    ]
 ];
