057c2217062d8ffc1700700714172d3f2ddd1c3a
alx <world.zay@gmail.com>
2025-06-21 12:04:22 +0300

    [feat] Create api_contract.php config

---
+++ application/api/config/api_contract.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'hosts' => [
+        'http://localhost',
+        'http://localhost:3000',
+        'http://localhost:5500',
+        'http://localhost:5173',
+    ],
+    'headers' => [
+        'Authorization',
+        'Content-Type',
+        'X-Bearer',
+        'X-Refresh',
+    ],
+    'methods' => [
+        'get',
+        'post',
+        'delete',
+        'patch',
+        'put',
+    ],
+    'max_age' => 3600,
+    'allow_credentials' => true,
+];
