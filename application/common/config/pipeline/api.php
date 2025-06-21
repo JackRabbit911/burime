33ae6c7acd16fc830ed129e3cc017738489f4dd2
alx <world.zay@gmail.com>
2025-06-21 13:25:41 +0300

    [feat] Update application/common/config/pipeline/api.php

--- application/common/config/pipeline/api.php
+++ application/common/config/pipeline/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Auth\Api\Middleware\AuthMiddleware;
+use Auth\Api\Middleware\O2AuthGuard;
 use Sys\I18n\I18nMiddleware;
 use Sys\Middleware\CORSMiddleware;
 
 $this->pipe(CORSMiddleware::class);
 $this->pipe(I18nMiddleware::class);
-$this->pipe(AuthMiddleware::class);
+$this->pipe(O2AuthGuard::class);
