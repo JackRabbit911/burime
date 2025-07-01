1fafbefb86f961f77f9f1952194fb1e0ec007864
alx <world.zay@gmail.com>
2025-07-01 16:08:12 +0300

    Create Auth\Command\FakeUsers class and console_commands config

---
+++ application/common/config/console_commands.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Auth\Command\FakeUsers;
+use Symfony\Component\Console\Command\Command;
+
+return [
+    'fake:users' => static fn(): Command => container()->get(FakeUsers::class),
+];
