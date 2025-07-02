f9256d0445fdbaf4e8c84cf3af536a7b2a4392d7
alx <world.zay@gmail.com>
2025-07-02 17:44:14 +0300

    [fix] Update /auth/Api/Middleware/AuthValidation.php

--- application/auth/Api/Middleware/AuthValidation.php
+++ application/auth/Api/Middleware/AuthValidation.php
@@ -47,7 +47,18 @@ final class AuthValidation extends ValidationMiddleware
 
         $response = [
             'success' => false,
-            'error' => $validationResponse,
+            'error' => [
+                'email' => [
+                    'status' => $validationResponse['email']['status'],
+                    'value' => $validationResponse['email']['value'],
+                    'message' => $validationResponse['email']['msg'],
+                ],
+                'password' => [
+                    'status' => $validationResponse['password']['status'],
+                    'value' => $validationResponse['password']['value'],
+                    'message' => $validationResponse['password']['msg'],
+                ],
+            ],
         ];
 
         return new JsonResponse($response, 200);
