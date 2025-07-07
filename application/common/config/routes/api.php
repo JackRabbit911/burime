c2825167b4738254cc3ad4d62e3ca411b1e44ec7
alx <world.zay@gmail.com>
2025-07-07 17:03:15 +0300

    #23 Update api_o2auth and routes/api configs

--- application/common/config/routes/api.php
+++ application/common/config/routes/api.php
@@ -1,14 +1,17 @@
 <?php declare(strict_types=1);
 
 // use Api\Controller\ApiTest;
+
+use Adm\Controller\Users;
 use App\Burime\Controller\DeletePostApi;
 use App\Rating\RatingApi;
 use Auth\Api\Controller\O2Auth;
 use Sys\Console\Controller as ConsoleController;
 
 return [
-    'console'   => ['/api/console/{model}/{method}', ConsoleController::class, ['model' => '[\w\/]+']],
-    'rating'    => ['/api/rating/{action}/{post_id}', RatingApi::class],
-    'post.confirm'=>['/api/post/{action}/{post_id}', DeletePostApi::class],
-    'api.auth'  => ['/api/auth/{action}', O2Auth::class],
+    'console'       => ['/api/console/{model}/{method}', ConsoleController::class, ['model' => '[\w\/]+']],
+    'rating'        => ['/api/rating/{action}/{post_id}', RatingApi::class],
+    'post.confirm'  => ['/api/post/{action}/{post_id}', DeletePostApi::class],
+    'api.auth'      => ['/api/auth/{action}', O2Auth::class],
+    'api.adm.users' => ['/api/adm/users', Users::class],
 ];
