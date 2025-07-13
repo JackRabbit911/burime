483450c43d1027da97a351904f8a3c3630a8ef89
alx <world.zay@gmail.com>
2025-07-13 18:02:11 +0300

    #23 Update admin module

--- application/adm/Controller/Users.php
+++ application/adm/Controller/Users.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace Adm\Controller;
 
-use Adm\Model\ModelUsers as ModelUsers;
+use Adm\Model\ModelUsers;
+use Adm\Middleware\SearchValidation;
 use Sys\Controller\ApiController;
 
+#[SearchValidation]
 class Users extends ApiController
 {
     public function __construct(private ModelUsers $model){}
 
     public function __invoke()
     {
-        return $this->model->get();
+        $params = $this->request->getQueryParams();
+        $filter = $params['name'] ?? null;
+        $page = $params['pageNumber'] ?? 1;
+        $limit = $params['perPage'] ?? 10;
+        $offset = ((int) $page - 1) * (int) $limit;
+
+        return $this->model->get((int) $limit, $offset, $filter);
     }
 }
