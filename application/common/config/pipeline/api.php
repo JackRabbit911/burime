6d2339741dd1a774aad996bccf185aed60857336
alx <world.zay@gmail.com>
2025-08-03 16:38:24 +0300

    #33 Update pipeline/api config

--- application/common/config/pipeline/api.php
+++ application/common/config/pipeline/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use Auth\Api\Middleware\O2AuthGuard;
-use Sys\I18n\I18nMiddleware;
 use Sys\Middleware\CORSMiddleware;
+use Sys\I18n\I18nMiddleware;
 
 $this->pipe(CORSMiddleware::class);
 $this->pipe(I18nMiddleware::class);
-$this->pipe(O2AuthGuard::class);
