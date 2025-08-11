fd1d52493e2eeb69a917cd04e0a11c1b5b2828b1
alx <world.zay@gmail.com>
2025-08-11 20:38:05 +0300

    #40 Create new component FinalControls

---
+++ front_src/branch/src/steps/FinalControls.tsx
@@ -0,0 +1,14 @@
+const FinalControls = () => {
+    return (
+        <div className="flex flex-row justify-between gap-2">
+            <button className="btn btn-primary">
+                Одно
+            </button>
+            <button className="btn btn-primary">
+                Другое
+            </button>
+        </div>
+    )
+}
+
+export default FinalControls
