7aa4501734f23d6a0c4fe0b723464000d4f24fad
alx <world.zay@gmail.com>
2025-08-19 11:54:40 +0300

    #40 Create InviteAuthors component

--- front_src/branch/src/steps/Authors/BranchAuthors.tsx
+++ front_src/branch/src/steps/Authors/BranchAuthors.tsx
@@ -1,36 +1,17 @@
 import { useList } from "effector-react";
 import { $branchAuthors } from "../../store/branch";
-import type { BranchAuthor } from "../../store/authors/types";
-import { authorRemoved, authorRoleToggled } from "../../store/authors";
+import InvitedAuthors from "./InvitedAuthors";
 
 const BranchAuthors = () => {
-  const deleteBranchAuthor = (author: BranchAuthor) => () => { authorRemoved(author) }
-  const changeAuthorRole = (author: BranchAuthor) => () => { authorRoleToggled(author) }
-
   const authors = useList($branchAuthors, (author) => (
-    <div className="flex flex-row justify-between gap-2">
-      <label className="fieldset-label flex justify-between w-full">
-        <legend className="fieldset-legend me-0.5 pb-1 pt-0">{author.alias}</legend>
-        <input
-          type="checkbox"
-          className="checkbox"
-          checked={author.role === 100}
-          onChange={changeAuthorRole(author)}
-        />
-      </label>
-      <button
-        className="btn btn-outline btn-square btn-xs rounded-lg"
-        onClick={deleteBranchAuthor(author)}
-      >
-        <span className=" text-red-600">X</span>
-      </button>
-    </div>
+    <InvitedAuthors author={author} />
   ))
 
   return (
     <>
-      <legend className="fieldset-legend">
-        Invited
+      <legend className="fieldset-legend flex justify-between">
+        <span>Alias</span>
+        <span className="me-4">Moderator</span>
       </legend>
       <div className="flex flex-col gap-2">
         {authors}
