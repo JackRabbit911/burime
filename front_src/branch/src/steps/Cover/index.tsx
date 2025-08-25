b8c759c077dff9adabe5d186e239a3cef66e77ce
alx <world.zay@gmail.com>
2025-08-25 17:21:15 +0300

    #40 Create Alert component

--- front_src/branch/src/steps/Cover/index.tsx
+++ front_src/branch/src/steps/Cover/index.tsx
@@ -2,11 +2,22 @@ import { useUnit } from "effector-react"
 import { $branch } from "../../store/branch"
 import BookCover from "./BookCover"
 import CoverControls from "./CoverControls"
+import { getMasterAlias } from "./utils"
+import Alert from "./Alert"
 
 const Cover = () => {
   const { authors, genres, title, info } = useUnit($branch)
+
+  const author = getMasterAlias(authors)
+
+  const authorExists = typeof author === 'undefined' ? false : true
+  const genresExists = genres.length === 0 ? false : true
+  const titleExists = !title ? false : true
+
+  console.log({authorExists, genresExists, titleExists})
   
-  return (
+  return ( authorExists && genresExists && titleExists )
+  ? (
     <div className="grid md:grid-cols-3 gap-4">
       <BookCover
         authors={authors}
@@ -16,7 +27,11 @@ const Cover = () => {
       />
       <CoverControls info={info} />
     </div>
-  )
+  ) : <Alert
+      authorExists={authorExists}
+      genresExists={genresExists}
+      titleExists={titleExists}
+    />
 }
 
 export default Cover
