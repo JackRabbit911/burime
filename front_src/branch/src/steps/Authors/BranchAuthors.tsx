503ee6ac1598e31e579e4ce8adaf84ba12207e5a
alx <world.zay@gmail.com>
2025-08-28 18:08:22 +0300

    #40 Hotfix Change html in BranchAuthors component

--- front_src/branch/src/steps/Authors/BranchAuthors.tsx
+++ front_src/branch/src/steps/Authors/BranchAuthors.tsx
@@ -9,10 +9,12 @@ const BranchAuthors = () => {
 
   return (
     <>
-      <legend className="fieldset-legend flex justify-between">
-        <span>Alias</span>
-        <span className="me-4">Moderator</span>
-      </legend>
+      <div>
+        <legend className="fieldset-legend flex justify-between">
+          <span>Alias</span>
+          <span className="me-4">Moderator</span>
+        </legend>
+      </div>
       <div className="flex flex-col gap-2">
         {authors}
       </div>
