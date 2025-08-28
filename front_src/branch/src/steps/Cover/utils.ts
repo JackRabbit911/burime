d8b9dd27cf0101ce75a2ece4aeecd580fad5d688
alx <world.zay@gmail.com>
2025-08-28 14:21:09 +0300

    #40 Update generate getre string function

--- front_src/branch/src/steps/Cover/utils.ts
+++ front_src/branch/src/steps/Cover/utils.ts
@@ -1,11 +1,14 @@
 import type { SameWeightGenres } from "../../store/bootstrap/types";
 
-export const getGenreString = (totalGenres: SameWeightGenres[], branchGenres: number[]) => {
-    const genreStr = [...totalGenres[1].genres, ...totalGenres[2].genres]
+export const getGenreString = (
+    totalGenres: SameWeightGenres[],
+    branchGenres: number[],
+) =>
+    [...totalGenres[1].genres, ...totalGenres[2].genres]
         .filter((genre) => branchGenres.includes(genre.id))
-        .map((genre) => genre.title).join(', ')
-
-    return genreStr ? genreStr : [...totalGenres[0].genres]
+        .map((genre) => genre.title)
+        .join(', ') ||
+    totalGenres[0].genres
         .filter((genre) => branchGenres.includes(genre.id))
-        .map((genre) => genre.title).join(', ')
-}
+        .map((genre) => genre.title)
+        .join(', ') || 'Unknown genre'
