4976023160fcd2374cff202935681fb0dcf09cb6
alx <world.zay@gmail.com>
2025-08-29 15:14:50 +0300

    #40 Update FinalControls conponent, add disabled attribute

--- front_src/branch/src/steps/FinalControls.tsx
+++ front_src/branch/src/steps/FinalControls.tsx
@@ -1,14 +1,29 @@
+import { useUnit } from "effector-react"
+import { $readyToPublish, $requiredFields } from "../store/validation"
+
 const FinalControls = () => {
-    return (
-        <div className="flex flex-row justify-between gap-2">
-            <button className="btn btn-primary">
-                Одно
-            </button>
-            <button className="btn btn-primary">
-                Другое
-            </button>
-        </div>
-    )
+  const { titleExists } = useUnit($requiredFields)
+  const readyToPublish = useUnit($readyToPublish)
+
+  return (
+    <div className="flex flex-row justify-between gap-2">
+      <button className="btn btn-error">
+        Cansel
+      </button>
+      <button
+        className="btn"
+        disabled={!titleExists}
+      >
+        Draft
+      </button>
+      <button
+        className="btn btn-primary dark:btn-info"
+        disabled={!readyToPublish}
+      >
+        Publish
+      </button>
+    </div>
+  )
 }
 
 export default FinalControls
