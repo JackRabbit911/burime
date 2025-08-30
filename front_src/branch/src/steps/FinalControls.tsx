6c2189d136555a4cebee38024184a5f382724297
alx <world.zay@gmail.com>
2025-08-30 11:22:32 +0300

    #40 Add redirect on Publish button

--- front_src/branch/src/steps/FinalControls.tsx
+++ front_src/branch/src/steps/FinalControls.tsx
@@ -5,24 +5,33 @@ const FinalControls = () => {
   const { titleExists } = useUnit($requiredFields)
   const readyToPublish = useUnit($readyToPublish)
 
+  const onPublish = () => {
+    window.location.href = '/private/books'
+  }
+
   return (
-    <div className="flex flex-row justify-between gap-2">
-      <button className="btn btn-error">
-        Cansel
-      </button>
-      <button
-        className="btn"
-        disabled={!titleExists}
-      >
-        Draft
-      </button>
-      <button
-        className="btn btn-primary dark:btn-info"
-        disabled={!readyToPublish}
-      >
-        Publish
-      </button>
-    </div>
+    <>
+      <div className="flex flex-row justify-between gap-2">
+        <button
+          className="btn btn-error"
+        >
+          Cansel
+        </button>
+        <button
+          className="btn"
+          disabled={!titleExists}
+        >
+          Draft
+        </button>
+        <button
+          className="btn btn-primary dark:btn-info"
+          disabled={!readyToPublish}
+          onClick={onPublish}
+        >
+          Publish
+        </button>
+      </div>
+    </>
   )
 }
 
