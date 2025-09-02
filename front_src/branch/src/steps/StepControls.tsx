28ffeafede3c79486e46284213c86e0037b27b6a
alx <world.zay@gmail.com>
2025-09-02 13:23:47 +0300

    #40 Add modal open button

--- front_src/branch/src/steps/StepControls.tsx
+++ front_src/branch/src/steps/StepControls.tsx
@@ -1,6 +1,7 @@
 import { useUnit } from "effector-react"
 import { $step, stepChanged } from "../store/common"
 import FinalControls from "./FinalControls"
+import { modalClicked } from "../reused/Dialog/store"
 
 const StepControls = () => {
     const step = useUnit($step)
@@ -18,6 +19,12 @@ const StepControls = () => {
             <button className="btn btn-primary dark:btn-info" onClick={onPrevStep} disabled={step===1}>
                 Prev
             </button>
+            <button
+          className="btn btn-link dark:text-info"
+          onClick={()=>{modalClicked(true)}}
+        >
+          More info...
+        </button>
             {step === 5 ? (
                 <FinalControls />
             ) : (
