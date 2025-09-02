0defccc483bbe48ef783cbaed85529c9e9b386a7
alx <world.zay@gmail.com>
2025-09-02 13:25:02 +0300

    #40 Add Dialog component

--- front_src/branch/src/steps/Genres.tsx
+++ front_src/branch/src/steps/Genres.tsx
@@ -3,38 +3,51 @@ import { $selectedGenres, genreToggled } from "../store/branch"
 import CheckBox from "../reused/CheckBox"
 import { $sameWeightGenres } from "../store/bootstrap"
 import { $requiredFields } from "../store/validation"
+import Dialog from "../reused/Dialog"
 
 const Genres = () => {
   const sameWeightGenres = useUnit($sameWeightGenres)
   const selectedGenres = useUnit($selectedGenres)
   const { genresExists } = useUnit($requiredFields)
-  const alert = genresExists ? '' : 'You need to choose at least one genre' 
+  const alert = genresExists ? '' : 'You need to choose at least one genre'
 
   return (
-    <fieldset className="fieldset">
-      <legend className="fieldset-legend flex justify-end w-full h-8">
-        {alert && <span className="label-text text-error">
-          {alert}
-        </span>}
-      </legend>
-      {sameWeightGenres.map(
-        ({ genres }, key) => (
-          <div className="flex flex-row flex-wrap gap-4" key={key}>
-            {key > 0 && (
-              <div className="divider w-full my-0"></div>
-            )}
-            {genres.map(({ id, title }) => (
-              <CheckBox
-                label={title}
-                value={id}
-                checked={selectedGenres.includes(id)}
-                onChange={genreToggled}
-              />
-            ))}
-          </div>
-        )
-      )}
-    </fieldset>
+    <>
+      <div className="collapse collapse-arrow border-base-300 border">
+        <input type="checkbox" />
+        <div className="collapse-title">How do I create an account?</div>
+        <div className="collapse-content text-sm">
+          Click the "Sign Up" button in the top right corner and follow the registration process.
+        </div>
+      </div>
+      <fieldset className="fieldset">
+        <legend className="fieldset-legend flex justify-end w-full h-8">
+          {alert && <span className="label-text text-error">
+            {alert}
+          </span>}
+        </legend>
+        {sameWeightGenres.map(
+          ({ genres }, key) => (
+            <div className="flex flex-row flex-wrap gap-4" key={key}>
+              {key > 0 && (
+                <div className="divider w-full my-0"></div>
+              )}
+              {genres.map(({ id, title }) => (
+                <CheckBox
+                  label={title}
+                  value={id}
+                  checked={selectedGenres.includes(id)}
+                  onChange={genreToggled}
+                />
+              ))}
+            </div>
+          )
+        )}
+      </fieldset>
+      <Dialog>
+        Ну и чего тебе не понятно про жанры?
+      </Dialog>
+    </>
   )
 }
 
--- front_src/branch/src/steps/Rules.tsx
+++ front_src/branch/src/steps/Rules.tsx
@@ -11,106 +11,99 @@ import {
   rwModeToggled,
   signatureToggled,
   timeLimitChanged,
-  titleChanged
 } from "../store/branch"
 import CheckBox from "../reused/CheckBox"
 import NumberInput from "../reused/NumberInput"
-import TextInput from "../reused/TextInput"
 import Textarea from "../reused/Textarea"
-import { $requiredFields } from "../store/validation"
+import Dialog from "../reused/Dialog"
 
 const Rules = () => {
   const branch = useUnit($branch);
-  const { titleExists } = useUnit($requiredFields)
-  const alert = titleExists ? '' : 'Required field'
 
   return (
-    <fieldset className="fieldset">
-      <div className="grid md:grid-cols-3 gap-4">
-        <div>
-          <legend className="fieldset-legend ms-0 mx-1 mb-2">
-            Read-write mode
-          </legend>
-          <RadioBox label={'Open'} value={0} checked={branch?.role === 0} onChange={rwModeToggled} />
-          <RadioBox label={'Open-Closed'} value={1} checked={branch?.role === 1} onChange={rwModeToggled} />
-          <RadioBox label={'Commercial'} value={2} checked={branch?.role === 2} onChange={rwModeToggled} />
-          <div className="divider my-1"></div>
-          <div className="flex flex-col gap-3">
-            <CheckBox
-              label={'Pre-moderation'}
-              value={branch?.info.moderation || 0}
-              checked={branch?.info.moderation === 1}
-              onChange={moderationToggled}
-            />
-            <CheckBox
-              label={'Allow comments'}
-              value={branch?.info.allow_comments || 1}
-              checked={branch?.info.allow_comments === 1}
-              onChange={allowCommentToggled}
-            />
-            <CheckBox
-              label={'Author`s signature under the post'}
-              value={branch?.info.signature || 0}
-              checked={branch?.info.signature === 1}
-              onChange={signatureToggled}
-            />
-            <div className="flex flex-row justify-between">
-              <NumberInput
-                label="Age Limit"
-                value={branch?.age_limit || 0}
-                minMaxStep={[0, 21, 3]}
-                onChange={ageLimitChanged}
+    <>
+      <fieldset className="fieldset">
+        <div className="grid md:grid-cols-3 gap-4">
+          <div>
+            <legend className="fieldset-legend ms-0 mx-1 mb-2">
+              Read-write mode
+            </legend>
+            <RadioBox label={'Open'} value={0} checked={branch?.role === 0} onChange={rwModeToggled} />
+            <RadioBox label={'Open-Closed'} value={1} checked={branch?.role === 1} onChange={rwModeToggled} />
+            <RadioBox label={'Commercial'} value={2} checked={branch?.role === 2} onChange={rwModeToggled} />
+            <div className="divider my-1"></div>
+            <div className="flex flex-col gap-3">
+              <CheckBox
+                label={'Pre-moderation'}
+                value={branch?.info.moderation || 0}
+                checked={branch?.info.moderation === 1}
+                onChange={moderationToggled}
               />
-            </div>
-            <div className="flex flex-row justify-between">
-              <NumberInput
-                label="Post Size"
-                value={branch?.info.post_size || 200}
-                minMaxStep={[50, 2000, 50]}
-                onChange={postSizeChanged}
+              <CheckBox
+                label={'Allow comments'}
+                value={branch?.info.allow_comments || 1}
+                checked={branch?.info.allow_comments === 1}
+                onChange={allowCommentToggled}
               />
-            </div>
-            <div className="flex flex-row justify-between">
-              <NumberInput
-                label="Time limit"
-                value={branch?.info.time_limit || 120}
-                minMaxStep={[30, 1440, 30]}
-                onChange={timeLimitChanged}
+              <CheckBox
+                label={'Author`s signature under the post'}
+                value={branch?.info.signature || 0}
+                checked={branch?.info.signature === 1}
+                onChange={signatureToggled}
               />
+              <div className="flex flex-row justify-between">
+                <NumberInput
+                  label="Age Limit"
+                  value={branch?.age_limit || 0}
+                  minMaxStep={[0, 21, 3]}
+                  onChange={ageLimitChanged}
+                />
+              </div>
+              <div className="flex flex-row justify-between">
+                <NumberInput
+                  label="Post Size"
+                  value={branch?.info.post_size || 200}
+                  minMaxStep={[50, 2000, 50]}
+                  onChange={postSizeChanged}
+                />
+              </div>
+              <div className="flex flex-row justify-between">
+                <NumberInput
+                  label="Time limit"
+                  value={branch?.info.time_limit || 120}
+                  minMaxStep={[30, 1440, 30]}
+                  onChange={timeLimitChanged}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="md:col-span-2">
-          <div className="flex flex-col gap-3">
-            <TextInput
-              label="Title"
-              value={branch?.title || ''}
-              placeholder="Введите название произведения"
-              optional="Up to 8 words"
-              alert={alert}
-              onChange={titleChanged}
-            />
-            <Textarea
-              label="Decription"
-              value={branch?.info.description || ''}
-              placeholder="Описание проекта"
-              rows={6}
-              optional="Up to 200 words"
-              onChange={decriptionChanged}
-            />
-            <Textarea
-              label="Extra rules"
-              value={branch?.info.rules || ''}
-              placeholder="Частные правила проекта"
-              rows={6}
-              optional="Up to 200 words"
-              onChange={rulesChanged}
-            />
+          <div className="md:col-span-2">
+            <div className="flex flex-col gap-3">
+              <Textarea
+                label="Decription"
+                value={branch?.info.description || ''}
+                placeholder="Описание проекта"
+                rows={7}
+                optional="Up to 200 words"
+                onChange={decriptionChanged}
+              />
+              <Textarea
+                label="Extra rules"
+                value={branch?.info.rules || ''}
+                placeholder="Частные правила проекта"
+                rows={7}
+                optional="Up to 200 words"
+                onChange={rulesChanged}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </fieldset>
+      </fieldset>
+      <Dialog>
+        Ну и чего тебе не понятно-то?
+      </Dialog>
+    </>
   )
 }
 
