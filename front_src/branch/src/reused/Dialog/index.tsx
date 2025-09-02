70382fd3029ef15a3639a6fb3255eabb4dc67123
alx <world.zay@gmail.com>
2025-09-02 13:16:27 +0300

    #40 Change id in modal block

--- front_src/branch/src/reused/Dialog/index.tsx
+++ front_src/branch/src/reused/Dialog/index.tsx
@@ -13,7 +13,7 @@ const Dialog = ({ children }: Props) => {
   }
 
   return (
-    <dialog id="my_modal_3" className="modal" open={isOpen}>
+    <dialog id="my_modal" className="modal" open={isOpen}>
       <div className="modal-box">
         <button
           className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
