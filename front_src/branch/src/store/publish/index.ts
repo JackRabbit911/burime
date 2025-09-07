13f7023be72a4e9e36c71dab8248d45cac63a62b
alx <world.zay@gmail.com>
2025-09-07 17:38:52 +0300

    #40 Create publish feature

--- front_src/branch/src/store/publish/index.ts
+++ front_src/branch/src/store/publish/index.ts
@@ -2,25 +2,32 @@ import { createEffect, createEvent, sample } from "effector";
 import ajax from "../../api/ajax";
 import type { ApiResponse } from "../../api/types";
 import { $branch } from "../branch";
-import type { Branch } from "../bootstrap/types";
+import type { Payload } from "./types";
+import { $posts } from "../posts";
+import { $bgFile, $coverFile } from "../common";
 // import { debug } from "patronum";
 
 export const published = createEvent()
 
-const sendFornDataFx = createEffect(
-    (branch: Branch) => {
-        return ajax.postForm<ApiResponse<Branch>>('branch/save', branch)
+const sendFormDataFx = createEffect(
+    (data: Payload) => {
+        return ajax.postForm<ApiResponse<Payload>>('branch/save', data)
     }
 )
 
 sample({
     clock: published,
-    source: $branch,
-    target: sendFornDataFx,
+    source: {
+        branch: $branch,
+        posts: $posts,
+        bg_img: $bgFile,
+        cover: $coverFile,
+    },
+    target: sendFormDataFx,
 })
 
 // const r = sample({
-//     clock: sendFornDataFx.doneData,
+//     clock: sendFormDataFx.doneData,
 // })
 
 // debug({r})
