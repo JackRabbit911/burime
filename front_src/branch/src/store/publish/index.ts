5087340a6ec6091274f448015f5b5d1cca6b94ce
alx <world.zay@gmail.com>
2025-09-09 13:14:41 +0300

    #40 Add insert branch_id to branch store after save

--- front_src/branch/src/store/publish/index.ts
+++ front_src/branch/src/store/publish/index.ts
@@ -1,17 +1,21 @@
-import { createEffect, createEvent, sample } from "effector";
-import ajax from "../../api/ajax";
-import type { ApiResponse } from "../../api/types";
-import { $branch } from "../branch";
+import { createEffect, createEvent, restore, sample } from "effector";
+import ajax from "api/ajax";
+import type { ApiResponse } from "api/types";
+import { $branch } from "store";
 import type { Payload } from "./types";
 import { $posts } from "../posts";
 import { $bgFile, $coverFile } from "../cover";
-// import { debug } from "patronum";
+import { debug } from "patronum";
 
 export const published = createEvent()
+export const allRightChanged = createEvent<boolean>()
+const branchIdRecived = createEvent<number>()
 
 const sendFormDataFx = createEffect(
-    (data: Payload) => ajax.postForm<ApiResponse<Payload>>('branch/save', data)
+    (data: Payload) => ajax.postForm<ApiResponse<number>>('branch/save', data)
 )
+
+export const $allRight = restore(allRightChanged, false)
 
 sample({
     clock: published,
@@ -24,8 +28,19 @@ sample({
     target: sendFormDataFx,
 })
 
-// const r = sample({
-//     clock: sendFormDataFx.doneData,
-// })
+sample({
+    clock: sendFormDataFx.doneData,
+    filter: (response) => Boolean(response?.data?.success),
+    target: allRightChanged.prepend(() => true),
+})
 
-// debug({r})
+sample({
+    clock: sendFormDataFx.doneData,
+    filter: (response) => Boolean(response?.data?.success),
+    fn: (response) => response?.data?.result,
+    target: branchIdRecived, 
+})
+
+$branch.on(branchIdRecived, (branch, id) => ({...branch, id}))
+
+debug({$allRight, allRightChanged})
