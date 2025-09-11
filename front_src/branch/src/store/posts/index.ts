d4e54f9c59006a0c5749e30c0da1772495ac286d
alx <world.zay@gmail.com>
2025-09-11 13:05:32 +0300

    #51 Update Posts type

--- front_src/branch/src/store/posts/index.ts
+++ front_src/branch/src/store/posts/index.ts
@@ -1,13 +1,36 @@
-import { createEvent, createStore } from "effector";
+import { createEvent, createStore, sample } from "effector";
 import type { Posts } from "./types";
 import { getBootstrapFx } from "../bootstrap";
 import { globalReset } from "store/common";
+import { debug } from "patronum";
 
 export const firstPostChanged = createEvent<string>()
 export const lastPostChanged = createEvent<string>()
 
-export const $posts = createStore<Posts>({first: '', last: ''})
-    .on(getBootstrapFx.doneData, (_, data) => data.result.posts)
-    .on(firstPostChanged, (store, first) => ({...store, first}))
-    .on(lastPostChanged, (store, last) => ({...store, last}))
+export const $posts = createStore<Posts>({
+    first: {id: null, body: ''},
+    last: {id: null, body: ''},
+}).on(getBootstrapFx.doneData, (_, data) => data.result.posts)
     .reset(globalReset)
+
+sample({
+    clock: firstPostChanged,
+    source: $posts,
+    fn: (posts, body) => ({
+        ...posts,
+        first: {...posts.first, body}
+    }),
+    target: $posts,
+})
+
+sample({
+    clock: lastPostChanged,
+    source: $posts,
+    fn: (posts, body) => ({
+        ...posts,
+        last: {...posts.last, body}
+    }),
+    target: $posts,
+})
+
+debug($posts)
