479ba293e694acde4bc1d58f28ca6a0355f91fc5
alx <world.zay@gmail.com>
2025-09-12 10:18:30 +0300

    #51 Create OwnerBranchGuard middleware

---
+++ application/app/Branch/Api/Middleware/OwnerBranchGuard.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Branch\Api\Middleware;
+
+use App\Branch\Api\Repository\BranchRepo;
+use Az\Route\Route;
+use HttpSoft\Response\EmptyResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class OwnerBranchGuard implements MiddlewareInterface
+{
+    public function __construct(private BranchRepo $repo){}
+
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler
+    ): ResponseInterface
+    {
+        $user = $request->getAttribute('user');
+        $route = $request->getAttribute(Route::class);
+        $id = $route->getParameters()['id'] ?? null;
+
+        if (!$id) {
+            return $handler->handle($request);
+        }
+        
+        $branch = $this->repo->findBranch((int)$id);
+
+        if ($branch->owner !== $user->id) {
+            return new EmptyResponse(403);
+        }
+
+        return $handler->handle($request->withAttribute('branch', $branch));
+    }
+}
