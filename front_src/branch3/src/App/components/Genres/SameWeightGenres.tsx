db8369b0eee4a364d80328e2ba2b9e50bde29566
alx <world.zay@gmail.com>
2025-10-09 16:20:47 +0300

    #64.1 Add key to genres list

--- front_src/branch3/src/App/components/Genres/SameWeightGenres.tsx
+++ front_src/branch3/src/App/components/Genres/SameWeightGenres.tsx
@@ -9,8 +9,9 @@ type Props = {
 
 const SameWeightGenres = ({ genres, checked, fieldName = 'genres' }: Props) => {
   return (
-    genres.map((option) => (
+    genres.map((option, key) => (
       <CheckBox
+        key={key}
         fieldName={fieldName}
         label={option.title}
         value={option.id}
