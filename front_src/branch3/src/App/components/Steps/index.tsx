36e158b0040a995b97605aaf1876740399b304bd
alx <world.zay@gmail.com>
2025-10-11 12:41:44 +0300

    #64.1 Create steps component

---
+++ front_src/branch3/src/App/components/Steps/index.tsx
@@ -0,0 +1,15 @@
+import Step from "./step";
+
+const Steps = () => {
+  return (
+    <ul className="steps w-full my-4">
+      <Step step={1} title="Genres" />
+      <Step step={2} title="Rules" />
+      <Step step={3} title="Authors" />
+      <Step step={4} title="Cover" />
+      <Step step={5} title="Publish" />
+    </ul>
+  );
+};
+
+export default Steps;
---
+++ front_src/branch3/src/App/components/Steps/step.tsx
@@ -0,0 +1,34 @@
+import { useUnit } from "effector-react";
+import { $darkMode } from "store/colorScheme";
+import { $step, stepChanged } from "store/step";
+
+type Props = {
+  step: number;
+  title: string;
+  isError?: boolean;
+};
+
+const Step = ({ step, title, isError = false }: Props) => {
+  const [currentStep, darkMode] = useUnit([$step, $darkMode]);
+  const color = darkMode ? 'step step-info' : 'step step-primary';
+  const liClassName = currentStep >= step ? color : 'step';
+
+  const btnClassName =
+    isError ?
+    'btn btn-link text-error' :
+    'btn btn-link dark:text-info';
+
+  const onStep = (newStep: number) => () => {
+    stepChanged(newStep);
+  }
+
+  return (
+    <li className={liClassName}>
+      <button className={btnClassName} onClick={onStep(step)}>
+        {title}
+      </button>
+    </li>
+  );
+};
+
+export default Step;
