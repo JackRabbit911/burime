f616da0988d340ba67b268ee3ba4ea8cf118addc
alx <world.zay@gmail.com>
2025-10-15 17:13:12 +0300

    #64.1 Add ErrorCmp component

--- front_src/branch3/src/App/index.tsx
+++ front_src/branch3/src/App/index.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import Form from "./components/Form"
-import { $bootstrap, appStarted } from "store/bootstrap";
+import { $bootstrap, $bootstrapStatus, appStarted } from "store/bootstrap";
 import { useUnit } from "effector-react";
 import { bootstrapSch } from "schema/input";
 import Loader from "reused/Loading";
+import ErrorCmp from "reused/ErrorCmp";
 
 const App = () => {
   const bootstrap = useUnit($bootstrap)
+  const status = useUnit($bootstrapStatus)
   
   useEffect(() => {
     appStarted()
@@ -19,6 +21,12 @@ const App = () => {
         console.log(valid.error)
         return <Loader message='Invalid input data' />
       }
+  }
+
+  if (status >= 400) {
+    return (
+        <ErrorCmp status={status} />
+    )
   }
 
   return bootstrap ? (
---
+++ front_src/branch3/src/reused/ErrorCmp.tsx
@@ -0,0 +1,34 @@
+import Wrapper from "./Wrapper";
+
+type Props = {
+  status: number;
+}
+
+const getReasonPhrase = (status: number) => {
+  switch (status) {
+    case 401:
+      return '401 | Unauthorized'
+    case 403:
+      return '403 | Forbidden'
+    case 404:
+      return '404 | Not found'
+    case 500:
+      return '500 | Internal server error'
+    case 503:
+      return '503 | Service Unavailable'
+    default:
+      return `${status} | Хз, что это...`
+  }
+}
+
+const ErrorCmp = ({ status }: Props) => (
+  <Wrapper>
+    <div className="h-96 flex flex-col justify-center">
+      <h1 className="text-center text-3xl">
+        {getReasonPhrase(status)}
+      </h1>
+    </div>
+  </Wrapper>
+)
+
+export default ErrorCmp
