4ae670466387c54b6fc8a893c695376b27c89054
alx <world.zay@gmail.com>
2025-10-15 16:17:52 +0300

    #64.1 Update BranchDTO, remove cover property

--- application/app/Branch/Api/BranchDTO.php
+++ application/app/Branch/Api/BranchDTO.php
@@ -14,7 +14,6 @@ class BranchDTO
     public readonly ?string $title;
     public readonly int $role;
     public readonly int $age_limit;
-    public readonly ?string $cover;
     public readonly stdClass $info;
     public readonly array $genres;
     public readonly array $authors;
@@ -27,7 +26,6 @@ class BranchDTO
         $this->title = $params['title'] ?? '';
         $this->role = $params['role'] ?? 0;
         $this->age_limit = $params['age_limit'] ?? 0;
-        $this->cover = $params['cover'] ?? null;
         $this->genres = $params['genres'] ?? [];
         $this->authors = $params['authors'] ?? [];
         
