262727e0e865b8198c04f84f82e436122962cc59
alx <world.zay@gmail.com>
2025-10-17 13:02:04 +0300

    #64.1 Update getDefaults function

--- front_src/branch3/src/App/components/Form/utils.ts
+++ front_src/branch3/src/App/components/Form/utils.ts
@@ -1,15 +1,27 @@
-import type { Branch } from "schema/input";
+import type { BranchAuthor, OwnAuthors } from "schema/authors";
+import type { Bootstrap } from "schema/input";
 
-export const getDefaults = (branch: Branch) => ({
-    branchTitle: branch.title || '',
-    genres: branch.genres,
-    branchRole: branch.role,
-    moderation: Boolean(branch.info.moderation),
-    comments: Boolean(branch.info.allow_comments),
-    signature: Boolean(branch.info.signature),
-    ageLimit: branch.age_limit,
-    postSize: branch.info.post_size,
-    timeLimit: branch.info.time_limit,
-    description: branch.info.description,
-    rules: branch.info.rules,
+export const getDefaults = (bootstrap: Bootstrap) => ({
+    branchTitle: bootstrap.branch.title || '',
+    genres: bootstrap.branch.genres,
+    branchRole: bootstrap.branch.role,
+    moderation: Boolean(bootstrap.branch.info.moderation),
+    comments: Boolean(bootstrap.branch.info.allow_comments),
+    signature: Boolean(bootstrap.branch.info.signature),
+    ageLimit: bootstrap.branch.age_limit,
+    postSize: bootstrap.branch.info.post_size,
+    timeLimit: bootstrap.branch.info.time_limit,
+    description: bootstrap.branch.info.description,
+    rules: bootstrap.branch.info.rules,
+    authors: bootstrap.branch.authors,
+    masterId: getMasterId(bootstrap.branch.authors, bootstrap.ownAuthors),
+    // ownAuthors: null,
 })
+
+export function getMasterId(authors: BranchAuthor[], ownAuthors: OwnAuthors) {
+    const master = authors.find(
+        ({ role }) => role >= 150
+    )
+
+    return !master ? ownAuthors[0].id : master.id
+}
