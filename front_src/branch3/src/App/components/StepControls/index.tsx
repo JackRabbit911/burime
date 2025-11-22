af8d93df3df084fad3b46daf971d7b4b995c1b01
alx <world.zay@gmail.com>
2025-11-22 13:32:27 +0300

    64.1 Add FinalControls component

--- front_src/branch3/src/App/components/StepControls/index.tsx
+++ front_src/branch3/src/App/components/StepControls/index.tsx
@@ -1,14 +1,14 @@
 import PrevButton from "./components/PrevButton";
 import NextButton from "./components/NextButton";
 import HelpButton from "./components/HelpButton";
-// import FinalControls from "./components/FinalControls";
+import FinalControls from "./components/FinalControls";
 
 const StepControls = () => (
   <div className="flex flex-row justify-between mt-4">
     <PrevButton />
     <HelpButton />
     <NextButton />
-    {/* <FinalControls /> */}
+    <FinalControls />
   </div>
 );
 
