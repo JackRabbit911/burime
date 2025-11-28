f252bbe4011da2859f7b6a11d12da8318fe6a536
alx <world.zay@gmail.com>
2025-11-28 12:33:04 +0300

    #64.1 Add MembersPermission component

--- front_src/branch3/src/App/components/Authors/index.tsx
+++ front_src/branch3/src/App/components/Authors/index.tsx
@@ -1,15 +1,22 @@
 import type { Bootstrap } from "schema/input"
 import AuthorsWrapper from "./AuthorsWrapper"
+import { useUnit } from "effector-react";
+import { $memberId } from "store/authors";
+import MembersPermissions from "./MembersPermissions";
 
 type Props = {
   bootstrap: Bootstrap;
 }
 
 const Authors = ({ bootstrap }: Props) => {
+  const memberId = useUnit($memberId)
 
   return (
     <div className="grid md:grid-cols-3 gap-4">
-      <AuthorsWrapper bootstrap={bootstrap} />
+      {memberId === 0
+        ? <AuthorsWrapper bootstrap={bootstrap} />
+        : <MembersPermissions id={memberId} />
+      }
     </div>
   )
 }
