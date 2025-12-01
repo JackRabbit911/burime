3cefa0d3fd54ab7d80eb245f340e0e062eb274b1
alx <world.zay@gmail.com>
2025-12-01 16:19:13 +0300

    #64.1 Add permissions and statuses mocks

--- front_src/branch3/src/App/components/Authors/permissions.ts
+++ front_src/branch3/src/App/components/Authors/permissions.ts
@@ -1,28 +1,9 @@
-export const permissions = {
-    'WRITE': 1 << 0,
-    'EDIT_POST': 1 << 1,
-    'DESIGN': 1 << 2,
-    'DIRECTOR': 1 << 3,
-    'MODERATE': 1 << 4,
-    'MANAGE': 1 << 5,
-    'EDIT_BRANCH': 1 << 6,
-    'EDIT_STATUS': 1 << 7,
-}
-
-export const memberStatus = { 
-    'deleted': 50,
-    'denied': 70,
-    'refused': 80,
-    'candidate': 90,
-    'invited': 110,
-    'invited_informed': 120,
-    'member': 200,
-}
+import type { Statuses } from "schema/input"
 
 export const isPermission = (role: number, permission: number) => (role & permission) !== 0 ? true : false
-export const moderatorPerm = permissions.MANAGE | permissions.MODERATE
-export const getStatus = (status: number) => (
-    Object.entries(memberStatus).reduce((acc, [key, value]) => {
+
+export const getStatusString = (statusObj: Statuses, status: number) => (
+    Object.entries(statusObj).reduce((acc, [key, value]) => {
         if (value === status) {
             acc = key
         }
