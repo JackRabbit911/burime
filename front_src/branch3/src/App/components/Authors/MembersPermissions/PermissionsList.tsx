36ec3cb96682fc21dca23bc63a24ea4ff3cbb191
alx <world.zay@gmail.com>
2025-12-01 16:24:13 +0300

    #64.1 Apply permissions from store

--- front_src/branch3/src/App/components/Authors/MembersPermissions/PermissionsList.tsx
+++ front_src/branch3/src/App/components/Authors/MembersPermissions/PermissionsList.tsx
@@ -1,30 +1,33 @@
 import { useFormContext } from "react-hook-form";
-import { isPermission, permissions } from "../permissions";
+import { isPermission } from "../permissions";
 import PermissionCheckBox from "./PermissionCheckBox";
 import type { Member } from "schema/authors";
 import { t } from "i18n/utils";
+import { useUnit } from "effector-react";
+import { $permissions } from "store/bootstrap";
 
 type Props = {
   member: Member | null;
 }
 
 const PermissionsList = ({ member }: Props) => {
+  const permissions = useUnit($permissions)
   const { setValue, getValues } = useFormContext()
 
   const members = getValues('members')
   const checked = (value: number): boolean => isPermission(member?.role || 0, value)
 
   const handleCheck = (val: number, id: number, isAdd: boolean) => {
-      const newAuthors = members.map((value: Member) => {
-        if (value.id === id) {
-          value.role = isAdd ? value.role | val : value.role &= ~ val
-        }
-  
-        return value
-      })
-  
-      setValue('members', newAuthors)
-    }
+    const newAuthors = members.map((value: Member) => {
+      if (value.id === id) {
+        value.role = isAdd ? value.role | val : value.role &= ~val
+      }
+
+      return value
+    })
+
+    setValue('members', newAuthors)
+  }
 
   return (
     <>
