c5bc8df73b8cc2844f181f6ba2fc463ea942de8a
alx <world.zay@gmail.com>
2025-12-01 16:22:23 +0300

    #64.1 Update and create utils files

--- front_src/branch3/src/App/components/Authors/utils.ts
+++ front_src/branch3/src/App/components/Authors/utils.ts
@@ -1,5 +1,4 @@
 import type { Author, Member, OwnAuthors } from "schema/authors"
-import { isPermission, memberStatus, permissions } from "./permissions"
 
 export const isInvited = (
     array: Member[],
@@ -13,8 +12,8 @@ export const isInvited = (
 export const addNewMember = (
     members: Member[],
     author: Author,
-    role: number = permissions.WRITE,
-    status: number = memberStatus.invited
+    role: number = 1,
+    status: number = 110
 ) => {
    const newMember = {
         id: author.id,
@@ -66,17 +65,3 @@ export const changeMaster = (members: Member[], ownAuthors: OwnAuthors, masterId
         return member
     })
 }
-
-export const isModerator = (member: Member | null): boolean =>
-    isPermission(member?.role || 0, permissions.MANAGE) &&
-    isPermission(member?.role || 0, permissions.MODERATE)
-
-
-export const isAccept = (member: Member | null): boolean =>
-    (member?.status || 0) === memberStatus.candidate ||
-    (member?.status || 0) === memberStatus.denied
-
-export const isBan = (member: Member | null): boolean =>
-    (member?.status || 0) === memberStatus.member &&
-    !isPermission(member?.role || 0, permissions.MANAGE) 
-
