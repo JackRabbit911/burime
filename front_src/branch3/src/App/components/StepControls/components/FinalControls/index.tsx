6c3131b2b054c3d8022d4625c969e116573a0d78
alx <world.zay@gmail.com>
2025-12-02 15:25:24 +0300

    #64.1 Add send data to sever feature

--- front_src/branch3/src/App/components/StepControls/components/FinalControls/index.tsx
+++ front_src/branch3/src/App/components/StepControls/components/FinalControls/index.tsx
@@ -3,19 +3,43 @@ import { useUnit } from "effector-react"
 import { useFormContext } from "react-hook-form";
 import { modalOpened } from "reused/Modal/store";
 import { $step } from "store/step"
-import { isObjectEmpty } from "utils";
 import CancelDialog from "../CancelDialog";
+import { formSchema } from "schema/output";
+import { draftClicked, published } from "store/publish";
 
 const FinalControls = () => {
   const step = useUnit($step)
-  const { watch, formState: { errors } } = useFormContext();
+  const [publishEvent, draftEvent] = useUnit([published, draftClicked])
+  const { watch } = useFormContext();
+
+  if (step < 5) {
+    return null
+  }
+
   const values = watch()
+  const valid = formSchema.safeParse(values)
+
+  if (!valid.success) {
+      console.log(valid.error, values)
+  }
 
   const onCancel = () => {
     modalOpened(<CancelDialog />)
   }
+  
+  const onPublish = () => {
+    if (valid.success) {
+      publishEvent(valid.data)
+    }
+  }
 
-  return step < 5 ? null : (
+  const onDraft = () => {
+    if (valid.success) {
+      draftEvent(valid.data)
+    }
+  }
+
+  return (
     <>
       <div className="flex flex-row justify-between gap-2">
         <button
@@ -26,14 +50,15 @@ const FinalControls = () => {
         </button>
         <button
           className="btn"
-          disabled={!isObjectEmpty(errors) || !values.branchTitle}
+          disabled={!valid.success || values.branchId}
+          onClick={onDraft}
         >
           Draft
         </button>
         <button
           className="btn btn-primary dark:btn-info"
-          disabled={!isObjectEmpty(errors) || !isReady(values)}
-        // onClick={onPublish}
+          disabled={!valid.success || !isReady(values)}
+          onClick={onPublish}
         >
           Publish
         </button>
