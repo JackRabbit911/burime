8c5be85bfeeacd9fce1dd5f34922419fd94b911a
alx <world.zay@gmail.com>
2025-12-03 12:05:31 +0300

    #64.1 Update Publish store

--- front_src/branch3/src/store/publish.ts
+++ front_src/branch3/src/store/publish.ts
@@ -1,4 +1,6 @@
+import type { AxiosError, AxiosResponse } from "axios";
 import { createEffect, createEvent, sample } from "effector";
+import { modalOpened } from "reused/Modal/store";
 import type { FormData } from "schema/output";
 import ajax from "services/ajax";
 import type { ApiResponse } from "services/ajax/types";
@@ -12,9 +14,9 @@ type Publish = z.infer<typeof publishSch>
 export const published = createEvent<FormData>()
 export const draftClicked = createEvent<FormData>()
 
-export const publishFx = createEffect(
+export const publishFx = createEffect<FormData, AxiosResponse<ApiResponse<string>>, AxiosError>(
     ( data: FormData ) => (
-        ajax.postForm<ApiResponse<Publish>>(uri, data)
+        ajax.postForm(uri, data)
     )
 )
 
@@ -32,4 +34,10 @@ sample({
 sample({
     clock: draftClicked,
     target: draftFx,
+})
+
+sample({
+    clock: publishFx.failData,
+    fn: (error) => error.message,
+    target: modalOpened,
 })
