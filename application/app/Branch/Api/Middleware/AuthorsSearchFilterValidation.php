22eba4f1f7721f366ce1c8973c0e2d444724115c
alx <world.zay@gmail.com>
2025-12-05 11:58:56 +0300

    #64.1 Update AuthorsSearchValidation

--- application/app/Branch/Api/Middleware/AuthorsSearchFilterValidation.php
+++ application/app/Branch/Api/Middleware/AuthorsSearchFilterValidation.php
@@ -4,37 +4,17 @@ declare(strict_types=1);
 
 namespace App\Branch\Api\Middleware;
 
-use Az\Validation\Middleware\ApiValidationMiddleware;
 use Common\Enum\MemberRole;
-use HttpSoft\Response\JsonResponse;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ResponseInterface;
 
-class AuthorsSearchFilterValidation extends ApiValidationMiddleware
+class AuthorsSearchFilterValidation extends ApiContractValidation
 {
     protected function setRules($request)
     {
         $whitelist = MemberRole::getFilters();
-        $this->validation->rule('filter', 'inArray', $whitelist)
+        $this->validation
+            ->rule('filter', 'inArray', $whitelist)
             ->rule('search', 'text_utf8')
             ->rule('page', 'integer')
             ->rule('limit', 'integer');
-    }
-
-    public function getResponse(
-        ServerRequestInterface $request,
-        RequestHandlerInterface $handler,
-        bool $check,
-    ): ResponseInterface
-    {
-        return $check
-            ? $handler->handle($request)
-            : (ENV > STAGE
-                ? new JsonResponse([
-                    'success' => false,
-                    'error' => $this->validation->getResponse(true),
-                ])
-                : new JsonResponse('Bad request', 400));
     }
 }
