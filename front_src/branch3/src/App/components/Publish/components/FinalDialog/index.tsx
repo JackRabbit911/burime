3f8cce9c6d7b9487c825c84543716c27097f5dd8
alx <world.zay@gmail.com>
2025-12-05 11:53:20 +0300

    #64.1 Update FinalDialog component

--- front_src/branch3/src/App/components/Publish/components/FinalDialog/index.tsx
+++ front_src/branch3/src/App/components/Publish/components/FinalDialog/index.tsx
@@ -4,7 +4,6 @@ import { useEffect } from "react"
 import type { FormData } from "schema/output"
 import { $finalResponse, draftClicked, published } from "store/publish"
 import DialogSuccess from "./components/DialogSuccess"
-import DialogError from "./components/DialogError"
 
 type Props = {
   data: FormData;
@@ -15,12 +14,13 @@ const Dialog = ({ data, draft = false }: Props) => {
   const [publishEvent, draftEvent] = useUnit([published, draftClicked])
   const finalResponse = useUnit($finalResponse)
 
+  console.log(finalResponse)
+
   useEffect(() => {
     draft ? draftEvent(data) : publishEvent(data)
   }, [])
 
-  if (!finalResponse) {
-    return (
+    return !finalResponse ? (
       <div className="flex flex-col justify-center min-h-64">
         <div className="text-center w-full">
           <span className="text-xl">
@@ -29,17 +29,12 @@ const Dialog = ({ data, draft = false }: Props) => {
           <span className="loading loading-dots loading-xs ms-1.5 mt-2.5"></span>
         </div>
       </div>
-    )
-  } else if (finalResponse.success) {
-    return (
+    ) : (
       <DialogSuccess
-        id={finalResponse?.result.id}
+        id={finalResponse?.id}
         draft={draft}
       />
     )
-  } else {
-    return <DialogError error={finalResponse?.error} />
-  }
 }
 
 export default Dialog
