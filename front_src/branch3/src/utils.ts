146c4566b7744484cdcc41d340ce17efa6e1aee1
alx <world.zay@gmail.com>
2025-12-06 13:37:06 +0300

    #64.1 Add getOjectProp function

--- front_src/branch3/src/utils.ts
+++ front_src/branch3/src/utils.ts
@@ -1,4 +1,4 @@
-export const isObjectEmpty = (obj : object) => {
+export const isObjectEmpty = (obj: object) => {
   for (const prop in obj) {
     if (Object.hasOwn(obj, prop)) {
       return false;
@@ -6,4 +6,20 @@ export const isObjectEmpty = (obj : object) => {
   }
 
   return true;
+}
+
+export const getObjectProp = (object: object, key: string) => {
+  let obj = object as any
+  const arr = key.split('.')
+  const f = arr.shift() as string
+
+  let m = obj[f]
+
+  arr.forEach((val) => {
+    if (m) {
+      m = m[val]
+    }
+  })
+
+  return m;
 }
