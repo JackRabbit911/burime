b3cd5fd0042e9a5b5a0ea8412ca49ed05bbf416d
alx <world.zay@gmail.com>
2025-12-06 13:39:29 +0300

    #64.1 Update getDefaults function

--- front_src/branch3/src/App/components/Form/utils.ts
+++ front_src/branch3/src/App/components/Form/utils.ts
@@ -7,28 +7,12 @@ export const getDefaults = (bootstrap: Bootstrap) => {
     const masterId = getMasterId(bootstrap.branch.members, bootstrap.ownAuthors)
     
     return {
-    branchId: bootstrap.branch.id,
-    branchTitle: bootstrap.branch.title || '',
-    genres: bootstrap.branch.genres,
-    branchRole: bootstrap.branch.role,
-    moderation: Boolean(bootstrap.branch.info.moderation),
-    comments: Boolean(bootstrap.branch.info.allow_comments),
-    signature: Boolean(bootstrap.branch.info.signature),
-    ageLimit: bootstrap.branch.age_limit,
-    postSize: bootstrap.branch.info.post_size,
-    timeLimit: bootstrap.branch.info.time_limit,
-    description: bootstrap.branch.info.description,
-    rules: bootstrap.branch.info.rules,
+    branch: bootstrap.branch,
+    posts: bootstrap.posts,
     masterId: masterId,
-    members: getMembers(bootstrap.branch.members, masterId, bootstrap.authorsStatuses.member),
-    bg_color: bootstrap.branch.info.bg_color,
-    text_size: bootstrap.branch.info.text_size,
-    text_color: bootstrap.branch.info.text_color,
     cover: base64ToFile(bootstrap.files.cover, 'cover'),
     bgImg: base64ToFile(bootstrap.files.bg_img, 'background'),
     authorsPayload: setAuthorsPayload(),
-    firstPost: bootstrap.posts.first.body || '',
-    lastPost: bootstrap.posts.last.body || '',
 }}
 
 export function setAuthorsPayload(limit = perPages[0]): AuthorsPayload {
@@ -51,17 +35,4 @@ function getMasterId(authors: Member[], ownAuthors: OwnAuthors) {
         })
         : null
     return !master ? ownAuthors[0].id : master.id
-}
-
-function getMembers(members: Member[], masterId: number, status: number) {
-    if (members.length === 0) {
-        members.push({
-            id: masterId,
-            role: 255,
-            status: status,
-            alias: ''
-        })
-    }
-
-    return members
 }
