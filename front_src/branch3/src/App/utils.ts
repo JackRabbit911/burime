fbc7534ac3586bc643e2960b93ce08907fdf3759
alx <world.zay@gmail.com>
2025-12-06 13:31:17 +0300

    #64.1 Add getOjectProp function

--- front_src/branch3/src/App/utils.ts
+++ front_src/branch3/src/App/utils.ts
@@ -2,17 +2,17 @@ import type { FieldValues } from "react-hook-form";
 import type { OwnAuthors } from "schema/authors";
 
 const readyCover = (values: FieldValues): boolean => (
-    values.cover
-    || values.bgImg
-    || values.bg_color !== '#eeeeee'
-    || values.text_color !== '#333333'
+    values.info.cover
+    || values.info.bgImg
+    || values.info.bg_color !== '#eeeeee'
+    || values.info.text_color !== '#333333'
 )
 
 export const readyProgress = (values: FieldValues) => {
-    const t = values.branchTitle ? 40 : 0
+    const t = values.title ? 40 : 0
     const g = values.genres.length > 0 ? 30 : 0
-    const d = values.description ? 15 : 0
-    const r = values.rules ? 10 : 0
+    const d = values.info.description ? 15 : 0
+    const r = values.info.rules ? 10 : 0
     const c = readyCover(values) ? 5 : 0 
 
     return t + g + d + r + c
@@ -21,7 +21,7 @@ export const readyProgress = (values: FieldValues) => {
 export const getAlerts = (values: FieldValues): string[] => {
     const result = []
 
-    if (!values.branchTitle) {
+    if (!values.title) {
         result.push('Title is required')
     }
 
@@ -29,11 +29,11 @@ export const getAlerts = (values: FieldValues): string[] => {
         result.push('You need to choose at least one genre')
     }
 
-    if (!values.description) {
+    if (!values.info.description) {
         result.push('Create a description for Your work')
     }
 
-    if (!values.rules) {
+    if (!values.info.rules) {
         result.push('Formulate the private rules of this branch')
     }
 
@@ -45,7 +45,7 @@ export const getAlerts = (values: FieldValues): string[] => {
 }
 
 export const isReady = (values: FieldValues): boolean => (
-    values.branchTitle && values.genres.length > 0
+    values.title && values.genres.length > 0
 )
 
 export const getMasterAlias = (ownAuthors: OwnAuthors, masterId: number) => (
