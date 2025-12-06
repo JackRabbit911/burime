29365feac1231e5bc998cf5371c92b8e097b5fda
alx <world.zay@gmail.com>
2025-12-06 13:36:12 +0300

    #64.1 Update input and output schemas

--- front_src/branch3/src/schema/input.ts
+++ front_src/branch3/src/schema/input.ts
@@ -1,27 +1,6 @@
 import * as z from "zod"
-import { ownAuthors } from "./authors"
-
-const info = z.strictObject({
-    moderation: z.number(),
-    allow_comments: z.number(),
-    signature: z.number(),
-    post_size: z.number(),
-    time_limit: z.number(),
-    description: z.string(),
-    rules: z.string(),
-    bg_color: z.string(),
-    text_color: z.string(),
-    text_size: z.number(),
-    cover: z.string(),
-    bg_img: z.string(),
-})
-
-const member = z.object({
-    id: z.number().positive(),
-    role: z.number().positive(),
-    status: z.number().positive(),
-    alias: z.string(),
-})
+import { member, ownAuthors } from "./authors"
+import { info, posts } from "./output"
 
 const genre = z.object({
     id: z.number().positive(),
@@ -33,17 +12,6 @@ const filesBase64 = z.object({
     bg_img: z.string().nullable(),
 })
 
-const post = z.object({
-    id: z.number().nullable(),
-    body: z.string(),
-})
-
-const posts = z.object({
-    first: post,
-    last: post,
-})
-
-const genresSch = z.array(z.array(genre))
 const genreSch = z.array(genre)
 
 const authorsFilters = z.array(z.string())
@@ -58,12 +26,12 @@ const branch = z.strictObject({
     role: z.number().nonnegative(),
     age_limit: z.number().nonnegative(),
     info: info,
-    genres: z.array(z.number().positive().nullable()),
+    genres: z.array(z.number().positive()),
     members: z.array(member),
 })
 
 export const bootstrapSch = z.object({
-    genres: genresSch,
+    genres: z.array(genreSch),
     branch: branch,
     posts: posts,
     files: filesBase64,
--- front_src/branch3/src/schema/output.ts
+++ front_src/branch3/src/schema/output.ts
@@ -8,37 +8,61 @@ export const branchTitle = z.string()
   .regex(/^[^<>;]*$/, 'Invalid input!')
   .refine((value) => value.trim().split(' ').length <= 3, 'Up to 3 words!')
 
+const regString = z.string().trim().regex(/^[^<>;]*$/, 'Invalid input!')
+
 const intro = (max: number) => z.string()
   .trim()
   .regex(/^[^<>;]*$/, 'Invalid input!')
   .refine((value) => value.trim().split(' ').length <= max, `Up to ${max} words!`)
 
-export const formSchema = z.object({
-  branchId: z.number().int().positive().nullable(),
-  branchTitle,
-  genres: z.array(z.coerce.number()).min(1, { message: "Please select at least one option." }),
-  branchRole: z.coerce.number().int().min(0).max(2),
-  moderation: z.boolean(),
-  comments: z.boolean(),
-  signature: z.boolean(),
-  ageLimit: z.coerce.number().int().nonnegative().max(21),
-  postSize: z.coerce.number().int().positive(),
-  timeLimit: z.coerce.number().int().positive(),
-  description: intro(200),
-  rules: intro(200),
-  masterId: z.coerce.number().positive(),
-  members: z.array(member),
+const post = z.object({
+    id: z.number().nullable(),
+    body: intro(200),
+})
+
+export const posts = z.object({
+    first: post,
+    last: post,
+})
+
+export const info = z.object({
+  allow_comments: z.coerce.number().min(0).max(1),
+  moderation: z.coerce.number().min(0).max(1),
+  signature: z.coerce.number().min(0).max(1),
+  post_size: z.number().int().positive(),
+  time_limit: z.number().int().positive(),
   bg_color: z.string().regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, "Invalid hex color format."),
-  text_size: z.coerce.number().int().min(5).max(50),
   text_color: z.string().regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, "Invalid hex color format."),
+  text_size: z.coerce.number().int().min(5).max(50),
+  bg_img: regString,
+  cover: regString,
+  rules: regString,
+  description: regString,
+})
+
+const branch = z.object({
+  id: z.number().int().positive().nullable(),
+  parent_id: z.number().int().positive().nullable(),
+  owner: z.number().int().positive().nullable(),
+  title: branchTitle,
+  role: z.coerce.number().int().nonnegative(),
+  age_limit: z.coerce.number().int().nonnegative().max(21),
+  info: info,
+  genres: z.array(z.coerce.number()).min(1, { message: "Please select at least one option." }),
+  members: z.array(member),
+})
+
+export const formSchema = z.object({
+  branch: branch,
+  posts: posts,
+  masterId: z.coerce.number().positive(),
   bgImg: imageFile,
   cover: imageFile,
   authorsPayload,
-  firstPost: intro(200),
-  lastPost: intro(200),
 });
 
 export const finalSchema = formSchema.omit({
+  masterId: true,
   authorsPayload: true,
 })
 
