33052c60647ab2eda23f1f8af13df438f1cd857d
alx <world.zay@gmail.com>
2025-12-06 13:47:19 +0300

    #64.1 Change fieldName

--- front_src/branch3/src/App/components/Rules/index.tsx
+++ front_src/branch3/src/App/components/Rules/index.tsx
@@ -12,40 +12,40 @@ const Rules = () => {
           <legend className="fieldset-legend ms-0 mx-1 mb-2">
             Read-write mode
           </legend>
-          <RadioBox label={'Open'} value={0} />
-          <RadioBox label={'Open-Closed'} value={1} />
-          <RadioBox label={'Commercial'} value={2} />
+          <RadioBox fieldName="branch.role" label={'Open'} value={0} />
+          <RadioBox fieldName="branch.role" label={'Open-Closed'} value={1} />
+          <RadioBox fieldName="branch.role" label={'Commercial'} value={2} />
           <div className="divider my-1"></div>
           <div className="flex flex-col gap-3">
             <CheckBox
-              fieldName={'moderation'}
+              fieldName={'branch.info.moderation'}
               label={t('Pre-moderation')}
             />
             <CheckBox
-              fieldName={'comments'}
+              fieldName={'branch.info.allow_comments'}
               label={t('Allow comments')}
             />
             <CheckBox
-              fieldName={'signature'}
+              fieldName={'branch.info.signature'}
               label={t('Author`s signature under the post')}
             />
             <div className="flex flex-row justify-between">
               <NumberInput
-                fieldName="ageLimit"
+                fieldName="branch.age_limit"
                 label={t("Age Limit")}
                 minMaxStep={[0, 21, 3]}
               />
             </div>
             <div className="flex flex-row justify-between">
               <NumberInput
-                fieldName="postSize"
+                fieldName="branch.info.post_size"
                 label={t("Post Size")}
                 minMaxStep={[50, 2000, 50]}
               />
             </div>
             <div className="flex flex-row justify-between">
               <NumberInput
-                fieldName="timeLimit"
+                fieldName="branch.info.time_limit"
                 label={t("Time limit")}
                 minMaxStep={[30, 1440, 30]}
               />
@@ -56,14 +56,14 @@ const Rules = () => {
         <div className="md:col-span-2">
           <div className="flex flex-col gap-3">
             <Textarea
-              fieldName="description"
+              fieldName="branch.info.description"
               label={t("Description")}
               placeholder="Описание проекта"
               rows={7}
               optional={t('Up to % words', 200)}
             />
             <Textarea
-              fieldName="rules"
+              fieldName="branch.info.rules"
               label={t("Extra rules")}
               placeholder="Частные правила проекта"
               rows={7}
