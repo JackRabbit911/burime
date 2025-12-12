2a5ce87bb470d72e2f245ec434035a27a156fb07
alx <world.zay@gmail.com>
2025-12-12 19:07:50 +0300

    #64.1 Update getDefaults function

--- front_src/branch3/src/App/components/Form/utils.ts
+++ front_src/branch3/src/App/components/Form/utils.ts
@@ -2,19 +2,22 @@ import { perPages } from "constants";
 import type { AuthorsPayload, Member, OwnAuthors } from "schema/authors";
 import { base64ToFile } from "schema/files";
 import type { Bootstrap } from "schema/input";
+import { changeMaster } from "../Authors/utils";
 
 export const getDefaults = (bootstrap: Bootstrap) => {
     const masterId = getMasterId(bootstrap.members, bootstrap.ownAuthors)
+    const members = getMembers(bootstrap.members, bootstrap.ownAuthors, masterId)
 
     return {
         branch: bootstrap.branch,
         branch_genres: bootstrap.branch_genres,
-        members: bootstrap.members,
+        members: members,
         posts: bootstrap.posts,
         masterId: masterId,
         cover: base64ToFile(bootstrap.files.cover, 'cover'),
         bgImg: base64ToFile(bootstrap.files.bg_img, 'background'),
         authorsPayload: setAuthorsPayload(),
+        draft: bootstrap.draft || null,
     }
 }
 
@@ -27,9 +30,9 @@ export function setAuthorsPayload(limit = perPages[0]): AuthorsPayload {
     }
 }
 
-function getMasterId(authors: Member[], ownAuthors: OwnAuthors) {
-    const master = authors.length > 0
-        ? authors.reduce((acc, val) => {
+function getMasterId(members: Member[], ownAuthors: OwnAuthors) {
+    const master = members.length > 0
+        ? members.reduce((acc, val) => {
             if (acc.role < val.role) {
                 acc = val
             }
@@ -38,4 +41,8 @@ function getMasterId(authors: Member[], ownAuthors: OwnAuthors) {
         })
         : null
     return !master ? ownAuthors[0].id : master.id
+}
+
+function getMembers(members: Member[], ownAuthors: OwnAuthors, masterId: number) {
+    return members.length === 0 ? changeMaster(members, ownAuthors, masterId) : members
 }
