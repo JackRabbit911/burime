87b3b0d5816dab589958a1c585b143487259fa3d
alx <world.zay@gmail.com>
2025-12-13 13:40:13 +0300

    #64.1 Update and remove Api conrtollers

--- application/app/Branch/Api/Controller/Branch.php
+++ application/app/Branch/Api/Controller/Branch.php
@@ -5,35 +5,31 @@ declare(strict_types=1);
 namespace App\Branch\Api\Controller;
 
 use App\Branch\Api\Middleware\OwnerBranchGuard;
-use App\Branch\Api\Repository\BranchRepo;
-use App\Branch\Api\Middleware\AuthGuard;
+use App\Branch\Api\Middleware\DraftBranchGetter;
 use App\Branch\Api\Middleware\AuthorsSearchFilterValidation;
-use App\Branch\Api\Middleware\BranchValidation;
-use App\Branch\Api\Repository\BranchSaveRepo;
-use App\Branch\Api\Repository\DraftSaveRepo;
+use App\Branch\Api\Middleware\AuthGuard;
+use App\Branch\Api\Repository\BranchRepo;
+use App\Branch\Api\Repository\HelpRepo;
 use Auth\Middleware\OAuthMiddleware;
 use Common\Enum\BranchAuthorPermissions;
 use Common\Enum\BranchAuthorStatus;
 use Az\Route\Route;
 use Common\Enum\MemberRole;
-use Sys\Middleware\PreparePostData;
 
 #[OAuthMiddleware]
 #[AuthGuard]
-#[OwnerBranchGuard]
 class Branch extends ApiContractController
 {
-    public function __construct(private BranchRepo $repo) {}
-
-    public function getbootstrap(?int $id = null)
+    #[Route(tokens: ['id' => '\d*', 'draft' => 'draft|'])]
+    #[DraftBranchGetter]
+    #[OwnerBranchGuard]
+    public function getbootstrap(?int $id = null, ?string $draft = null)
     {
-        $data['branch'] = $this->request->getAttribute('branch') ?? $this->repo->findBranch($id);
-        $data['total_genres'] = $this->repo->getTotalGenres();
-        $data['branch_genres'] = $this->repo->getBranchGenres($id);
-        $data['members'] = $this->repo->getBranchAuthors($id);
-        $data['posts'] = $this->repo->getFirstLastPosts($id);
-        $data['files'] = $this->repo->getBase64CoverFiles($id);
-        $data['ownAuthors'] = $this->repo->getOwnAuthors($this->user->id);
+        $repo = $this->request->getAttribute('repo');
+        $data = $this->request->getAttribute('branch');
+        $data['files'] = $repo->getBase64CoverFiles($id);
+        $data['total_genres'] = $repo->getTotalGenres();
+        $data['ownAuthors'] = $repo->getOwnAuthors($this->user->id);
         $data['authorsFilters'] = MemberRole::getFilters();
         $data['authorsPermissions'] = BranchAuthorPermissions::getArray();
         $data['authorsStatuses'] = BranchAuthorStatus::getArray();
@@ -42,16 +38,23 @@ class Branch extends ApiContractController
     }
 
     #[AuthorsSearchFilterValidation]
-    public function getauthors()
+    public function getauthors(BranchRepo $repo)
     {
         $query_params = $this->request->getQueryParams();
 
-        [$authors_count, $authors_list] = $this->repo->getAuthors($this->user->id, $query_params);
+        [$authors_count, $authors_list] = $repo->getAuthors($this->user->id, $query_params);
 
         return [
             'list' => $authors_list,
             'count' => $authors_count,
         ];
+    }
+
+    public function gethelp(HelpRepo $repo, int $step)
+    {
+        $help = $repo->getHelp($step);
+
+        return $help ? $help : $this->_error('Not found', 404);
     }
 
     #[Route(methods: 'post')]
@@ -61,23 +64,5 @@ class Branch extends ApiContractController
         $data = json_decode($json);
 
         return $this->i18n->getMap($data->filter);
-    }
-
-    #[Route(methods: 'post')]
-    #[PreparePostData]
-    #[BranchValidation]
-    public function save()
-    {
-        $draft = $this->request->getQueryParams()['draft'] ?? null;
-
-        $repo = $draft ? DraftSaveRepo::class : BranchSaveRepo::class;
-        $repo = container()->get($repo);
-
-        $post = $this->request->getParsedBody();
-        $files = $this->request->getUploadedFiles();
-
-        $branch_id = $repo->save($post, $files, $this->user->id);
-
-        return ['id' => $branch_id];
     }
 }
--- application/app/Branch/Api/Controller/BranchSave.php
+++ application/app/Branch/Api/Controller/BranchSave.php
@@ -4,25 +4,38 @@ declare(strict_types=1);
 
 namespace App\Branch\Api\Controller;
 
-use App\Branch\Api\Middleware\SantizeFormData;
-use App\Branch\Api\Middleware\BranchValidation;
 use App\Branch\Api\Repository\BranchSaveRepo;
+use App\Branch\Api\Repository\DraftSaveRepo;
+use App\Branch\Api\Middleware\BranchValidation;
 use App\Branch\Api\Middleware\AuthGuard;
 use Auth\Middleware\OAuthMiddleware;
 use Az\Route\Route;
+use Sys\Middleware\PreparePostData;
 
 #[OAuthMiddleware]
 #[AuthGuard]
 #[Route(methods: 'post')]
-#[SantizeFormData]
+#[PreparePostData]
 #[BranchValidation]
 class BranchSave extends ApiContractController
 {
     public function __invoke(BranchSaveRepo $repo)
     {
+        return $this->saveHandler($repo);
+    }
+
+    public function draft(DraftSaveRepo $repo)
+    {
+        return $this->saveHandler($repo);
+    }
+
+    private function saveHandler($repo)
+    {
         $post = $this->request->getParsedBody();
         $files = $this->request->getUploadedFiles();
 
-        return $repo->save($post, $files, $this->user->id);
+        $branch_id = $repo->save($post, $files, $this->user->id);
+
+        return ['id' => $branch_id];
     }
 }
