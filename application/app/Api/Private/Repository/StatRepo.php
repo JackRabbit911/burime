cb47f273c48b2b8a88588410762feb0380654984
alx <world.zay@gmail.com>
2025-12-25 14:05:27 +0300

    #84 Create complete profile feature

--- application/app/Api/Private/Repository/StatRepo.php
+++ application/app/Api/Private/Repository/StatRepo.php
@@ -8,13 +8,15 @@ use App\Api\Private\Model\ModelAuthors;
 use App\Api\Private\Model\ModelBooks;
 use App\Api\Private\Model\ModelDrafts;
 use App\Api\Private\StatDTO;
+use Common\Model\ModelUserStat;
 
 class StatRepo
 {
     public function __construct(
         private ModelAuthors $modelAuthors,
         private ModelBooks $modelBooks,
-        private ModelDrafts $modelDrafts
+        private ModelDrafts $modelDrafts,
+        private ModelUserStat $modelUserStat,
     ) {}
 
     public function get(int $user_id, array $ownAuthorsIds)
@@ -29,6 +31,18 @@ class StatRepo
                 'total' => $this->modelAuthors->getGroupsCount($ownAuthorsIds),
                 'own' => count($ownAuthorsIds),
             ],
+            'messages' => $this->modelUserStat->getMsgCount($ownAuthorsIds),
+            'complete' => $this->getComplete($user_id),
         ]);
+    }
+
+    private function getComplete(int $user_id)
+    {
+        $path = config('user', 'avatar_path') . $user_id;
+        $pattern = $path . '.{jpg,jpeg,png,gif}';
+        $file = glob($pattern, GLOB_BRACE)[0] ?? null;
+        $avatar_complete = $file ? 15 : 0;
+
+        return $this->modelUserStat->getUserComplete($user_id) + $avatar_complete;
     }
 }
