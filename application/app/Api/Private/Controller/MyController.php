6669528b013060b013a6a6e3f48ac602c5b0a0b1
alx <world.zay@gmail.com>
2026-01-10 14:02:43 +0300

    #88 Update books function

--- application/app/Api/Private/Controller/MyController.php
+++ application/app/Api/Private/Controller/MyController.php
@@ -29,7 +29,7 @@ class MyController extends ApiContractController
 
     public function books(ModelBooks $model)
     {
-        return $model->get($this->user->id, $this->ownAuthorsIds);
+        return $model->get($this->ownAuthorsIds);
     }
 
     public function authors(MyAuthorsRepo $repo)
