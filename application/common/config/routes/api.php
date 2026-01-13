117ce1dd3f2a1496c81fc3c62fbf40e70104d3d3
alx <world.zay@gmail.com>
2026-01-13 17:10:17 +0300

    #88 Add api.authors route

--- application/common/config/routes/api.php
+++ application/common/config/routes/api.php
@@ -5,6 +5,7 @@ use App\Api\Author\Controller\AuthorSave;
 use App\Api\Author\Controller\Group;
 use App\Api\Branch\Controller\BranchSave;
 use App\Api\Branch\Controller\MyBranch;
+use App\Api\Common\Controller\Authors;
 use App\Api\Common\Controller\Help;
 use App\Api\Common\Controller\ReferenceBooks;
 use App\Api\Common\Controller\Translate;
@@ -32,11 +33,13 @@ return [
     'api.help'      => ['/api/my/help/{path}', Help::class, ['path' => '.*']],
     'api.translate' => ['/api/my/gettranslate', Translate::class],
     'api.reference' => ['/api/my/reference/{action}', ReferenceBooks::class],
+    'api.authors'   => ['/api/my/authors/{action}/{id?}', Authors::class],
 
     'api.branchsave'=> ['/api/my/branch/action/{action}/{id?}', BranchSave::class],
     'api.my.branch' => ['/api/my/branch/{action}/{id?}/{draft?}', MyBranch::class],
 
     'api.my.group'  => ['/api/my/group/{action}/{id?}', Group::class],
     'api.authorsave'=> ['/api/my/author/{action}/{id?}', AuthorSave::class],
+
     'api.my'        => ['/api/my/{action}/{id?}', MyController::class],
 ];
