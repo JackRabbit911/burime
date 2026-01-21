045568bfd944f4bea8b90f439fb1fe76b2babaa9
alx <world.zay@gmail.com>
2026-01-21 12:43:54 +0300

    #90 Update getMessage and add deleteMsg functions

--- application/app/Api/Message/Repository/MsgRepo.php
+++ application/app/Api/Message/Repository/MsgRepo.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Api\Message\Repository;
 
 use App\Api\Common\Model\ModelAuthors;
+use App\Api\Message\Enum\MsgStatus;
 use App\Api\Message\Model\ModelMessage;
 
 class MsgRepo
@@ -24,14 +25,39 @@ class MsgRepo
         return $data;
     }
 
-    public function getMessage(int $id)
+    public function getMessage(int $id, int $user_id)
     {
-        $message = $this->modelMessage->find($id);
+        $ids = $this->modelAuthors->getOwnAuthorsIds($user_id);
+        
+        if (!$this->modelMessage->isOut($id, $ids)) {
+            $message = $this->modelMessage->findIncoming($id, $ids);
+            $message->incoming = true;
+            $this->modelMessage->setStatus($id, $ids, MsgStatus::Read->value);
+        } else {
+            $message = $this->modelMessage->findSended($id, $ids);
+
+            if (!$message) {
+                return null;
+            }
+
+            $message->incoming = false;
+        }
+
 
         if (json_validate($message->data)) {
             $message->data = json_decode($message->data);
         }
 
         return $message;
+    }
+
+    public function deleteIncommingMessage(int $id, int $recipient)
+    {
+        $this->modelMessage->deleteMsgLink($id, $recipient);
+    }
+
+    public function deleteSendedMessage(int $id)
+    {
+        $this->modelMessage->deleteMsg($id);
     }
 }
