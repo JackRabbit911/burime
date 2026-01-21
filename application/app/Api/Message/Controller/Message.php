47f6594cd22450f3b1d1693f58a57c4a7a9dc701
alx <world.zay@gmail.com>
2026-01-21 12:46:24 +0300

    #90 Add new delete functions and update show function

--- application/app/Api/Message/Controller/Message.php
+++ application/app/Api/Message/Controller/Message.php
@@ -6,6 +6,8 @@ namespace App\Api\Message\Controller;
 
 use App\Api\Common\Controller\ApiContractController;
 use App\Api\Message\Repository\MsgRepo;
+use Az\Route\Route;
+use HttpSoft\Response\EmptyResponse;
 
 class Message extends ApiContractController
 {
@@ -18,6 +20,24 @@ class Message extends ApiContractController
 
     public function show(int $id)
     {
-        return $this->repo->getMessage($id);
+        $message = $this->repo->getMessage($id, $this->user->id);
+
+        return $message ?: new EmptyResponse(404);
+    }
+
+    #[Route(methods: 'delete')]
+    public function delete(int $id, int $recipient)
+    {
+        $this->repo->deleteIncommingMessage($id, $recipient);
+
+        return "Message $id for $recipient was deleted";
+    }
+
+    #[Route(methods: 'delete')]
+    public function remove(int $id)
+    {
+        $this->repo->deleteSendedMessage($id);
+        
+        return "Message $id was removed";
     }
 }
