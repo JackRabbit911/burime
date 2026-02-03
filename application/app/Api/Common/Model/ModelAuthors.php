57ceee04e97327d17fc9bfe0e42f78d21cff2bc5
alx <world.zay@gmail.com>
2026-02-03 13:53:15 +0300

    #95 Add getByFilter function

--- application/app/Api/Common/Model/ModelAuthors.php
+++ application/app/Api/Common/Model/ModelAuthors.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Api\Common\Model;
 
+use Common\Enum\MemberRole;
 use Sys\Model\MysqlModel;
 use PDO;
 
@@ -33,5 +34,45 @@ class ModelAuthors extends MysqlModel
             ->select('id', 'alias')
             ->where('owner', '=', $user_id)
             ->get();
+    }
+
+    public function getByFilter(
+        int $user_id,
+        int $limit,
+        int $offset,
+        ?string $filter = null,
+        ?string $search = null,
+        array $except = []
+    )
+    {
+        $role = MemberRole::getByFilter($filter);
+
+        $table = $this->qb->table('authors')
+            ->selectDistinct('id')
+            ->select('alias');
+
+        if ($role > 1) {
+            $table->join('users_authors', 'users_authors.author_id', '=', 'authors.id')
+                ->where('role', '=', $role)
+                ->where('user_id', '=', $user_id);
+        } elseif ($role === 1) {
+            $table->where('authors.openclosed', '<', 2);
+        } else {
+            $table->where('authors.openclosed', '=', 2);
+        }
+
+        if ($search) {
+            $table->where('alias', 'LIKE', "%$search%");
+        }
+
+        if (!empty($except)) {
+            $table->whereNotIn('id', $except);
+        }
+
+        $count = $table->count();
+        $authors = $table->limit($limit)
+            ->offset($offset)->get();
+
+        return [$count, $authors];
     }
 }
