62f1185cc4c810c8cb6abafeebf4dab4fd28ee49
alx <world.zay@gmail.com>
2026-02-04 13:17:08 +0300

    #95 Add getStatus and setStatus functions

--- application/app/Api/Author/Model/ModelGroup.php
+++ application/app/Api/Author/Model/ModelGroup.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Api\Author\Model;
 
 use Sys\Model\MysqlModel;
+use PDO;
 
 class ModelGroup extends MysqlModel
 {
-    public function getMembers($group_id)
+    public function getMembers(int $group_id): array
     {
         return $this->qb->table(['authors_authors' => 'aa'])
             ->select($this->qb->raw('aa.child_id as id'))
@@ -16,5 +17,31 @@ class ModelGroup extends MysqlModel
             ->join('authors', 'authors.id', '=', 'child_id')
             ->where('parent_id', '=', $group_id)
             ->get();
+    }
+
+    public function getStatus(int $parent_id, int $child_id)
+    {
+        return $this->qb->table('authors_authors')
+            ->select('status')
+            ->where('parent_id', '=', $parent_id)
+            ->where('child_id', '=', $child_id)
+            ->setFetchMode(PDO::FETCH_COLUMN)
+            ->first();
+    }
+
+    public function setStatus(array $data)
+    {
+        $table = $this->qb->table('authors_authors');
+
+        if (!isset($data['role'])) {
+            $data['role'] = $table->select('role')
+                ->where('parent_id', '=', $data['parent_id'])
+                ->where('child_id', '=', $data['child_id'])
+                ->setFetchMode(PDO::FETCH_COLUMN)
+                ->first();
+        }
+
+        $table->onDuplicateKeyUpdate($data)
+            ->insert($data);
     }
 }
