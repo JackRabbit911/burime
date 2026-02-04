99179da796e58b3bdee2521b889564003c15ac54
alx <world.zay@gmail.com>
2026-02-04 13:19:30 +0300

    #95 Add getsatatus and setstatus functions

--- application/app/Api/Author/Controller/Group.php
+++ application/app/Api/Author/Controller/Group.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Api\Author\Controller;
 
 use App\Api\Author\Model\ModelGroup;
+use App\Api\Author\Middleware\StatusValidation;
 use App\Api\Common\Controller\ApiContractController;
+use Sys\Middleware\PreparePostData;
+use Az\Route\Route;
 
 class Group extends ApiContractController
 {
@@ -14,5 +17,24 @@ class Group extends ApiContractController
     public function members($id = null)
     {
         return $id ? $this->model->getMembers($id) : [];
+    }
+
+    public function getstatus($id)
+    {
+        $author_id = $this->request->getQueryParams()['author'];
+        $status = $this->model->getStatus((int) $id, (int) $author_id);
+
+        return ['status' => $status];
+    }
+
+    #[Route(methods: 'post')]
+    #[PreparePostData]
+    #[StatusValidation]
+    public function setstatus()
+    {
+        $post = $this->request->getParsedBody();
+        $this->model->setStatus($post);
+
+        return ['status' => $post['status']];
     }
 }
