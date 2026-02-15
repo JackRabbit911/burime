7e26864b2c0cfc58ca69a80d3e50e07aa074805c
alx <world.zay@gmail.com>
2026-02-15 16:21:54 +0300

    #109 Add and remove auth routes

--- application/auth/config/routes.php
+++ application/auth/config/routes.php
@@ -12,12 +12,12 @@ use Auth\Controller\OAuth;
 return [
     // 'demo'          => ['/', Demo::class],
     // 'auth'          => ['/auth/{action}', Auth::class],
-    'auth'          => ['/auth/{action}', OAuth::class],
-    'register'      => ['/auth/register/{action}/{code?}', Register::class],
-    'restore'       => ['/auth/restore/{action}/{code?}', Restore::class],
-    'restore.password'=>['/auth/restore/change/password/{action}', ChangePassword::class],
+    // 'auth'          => ['/auth/{action}', OAuth::class],
+    // 'register'      => ['/auth/register/{action}/{code?}', Register::class],
+    // 'restore'       => ['/auth/restore/{action}/{code?}', Restore::class],
+    // 'restore.password'=>['/auth/restore/change/password/{action}', ChangePassword::class],
 
-    'profile'       => ['/private/profile', [Profile::class, 'form']],
-    'profile.save'  => ['/private/profile/save', [Profile::class, 'save']],
-    'profile.password'=>['/private/change/password/{action}', ChangePassword::class]
+    // 'profile'       => ['/private/profile', [Profile::class, 'form']],
+    // 'profile.save'  => ['/private/profile/save', [Profile::class, 'save']],
+    // 'profile.password'=>['/private/change/password/{action}', ChangePassword::class]
 ];
