482af4ff83367c9a88a5b74e29acf413f7e38c92
alx <world.zay@gmail.com>
2026-02-16 15:25:17 +0300

    #109 Apply AuthMiddleware and update return

--- application/app/Api/Auth/Controller/Auth.php
+++ application/app/Api/Auth/Controller/Auth.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 namespace App\Api\Auth\Controller;
 
 use Az\Route\Route;
+use App\Api\Auth\Middleware\AuthMiddleware;
 
 class Auth extends ApiAuthController
 {
     public function __construct(){}
 
     #[Route(methods: 'post')]
+    #[AuthMiddleware]
     public function __invoke()
     {
-        $data = $this->request->getBody()->getContents();
-        $data = json_decode($data);
-
-        return $data;
+        return $this->request->getAttribute('user') ? true : false;
     }
 }
