ce1bb44bc5946b0f5c808ee3372b8e963802f86d
alx <world.zay@gmail.com>
2026-02-16 16:39:11 +0300

    #109 Add lodout function

--- application/app/Api/Auth/Controller/Auth.php
+++ application/app/Api/Auth/Controller/Auth.php
@@ -6,6 +6,8 @@ namespace App\Api\Auth\Controller;
 
 use Az\Route\Route;
 use App\Api\Auth\Middleware\AuthMiddleware;
+use App\Api\Auth\Service\OAuth;
+use HttpSoft\Response\RedirectResponse;
 
 class Auth extends ApiAuthController
 {
@@ -16,5 +18,14 @@ class Auth extends ApiAuthController
     public function __invoke()
     {
         return $this->request->getAttribute('user') ? true : false;
+    }
+
+    public function logout(OAuth $oauth)
+    {
+        $oauth->logout();
+
+        return true;
+
+        // return new RedirectResponse('/');
     }
 }
