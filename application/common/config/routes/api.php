b91872a04dc3ca30bf33b1fcf298e44591ad1101
alx <world.zay@gmail.com>
2026-02-17 11:54:04 +0300

    #109 Update api/auth route

--- application/common/config/routes/api.php
+++ application/common/config/routes/api.php
@@ -26,7 +26,7 @@ return [
     'console'       => ['/api/console/{model}/{method}', ConsoleController::class, ['model' => '[\w\/]+']],
     'rating'        => ['/api/rating/{action}/{post_id}', RatingApi::class],
     'post.confirm'  => ['/api/post/{action}/{post_id}', DeletePostApi::class],
-    'api.auth'      => ['/api/auth/{action}', O2Auth::class],
+    // 'api.auth'      => ['/api/auth/{action}', O2Auth::class],
     'api.adm.users' => ['/api/adm/users/{id?}', Users::class],
 
     // 'branch.help'   => ['/api/branch/help/{step}', [Branch::class, 'gethelp']],
