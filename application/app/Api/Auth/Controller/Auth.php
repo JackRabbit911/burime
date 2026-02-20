3cdeb3c2d50f39056ce83ad3b999a65b76c6375d
alx <world.zay@gmail.com>
2026-02-20 11:29:22 +0300

    #111 Add confirm function and add send email and setCode features

--- application/app/Api/Auth/Controller/Auth.php
+++ application/app/Api/Auth/Controller/Auth.php
@@ -6,10 +6,11 @@ namespace App\Api\Auth\Controller;
 
 use App\Api\Auth\Service\OAuth;
 use App\Api\Auth\Service\TokenAuth;
+use App\Api\Auth\Model\ModelRecovery;
 use App\Api\Auth\Middleware\AuthMiddleware;
 use App\Api\Auth\Middleware\AuthValidation;
 use App\Api\Auth\Middleware\EmailValidation;
-use App\Api\Auth\Model\ModelRecovery;
+use App\Api\Auth\Service\SendEmail;
 use Az\Route\Route;
 
 class Auth extends ApiAuthController
@@ -31,12 +32,24 @@ class Auth extends ApiAuthController
 
     #[Route(methods: 'post')]
     #[EmailValidation]
-    public function email(ModelRecovery $model)
-    {
+    public function email(
+        ModelRecovery $model,
+        SendEmail $mailer
+    ) {
         $data = $this->request->getBody()->getContents();
         $data = json_decode($data);
+
         $user = $model->findByEmail($data->email);
+        $code = bin2hex(random_bytes(16));
+
+        $model->setCode($code);
+        $mailer->recovery($this->request->getUri(), $user, $this->i18n->lang(), $code);
 
         return $user;
+    }
+
+    public function confirm(ModelRecovery $model, string $code)
+    {
+        return $model->getCode($code) === $code ?: false;
     }
 }
