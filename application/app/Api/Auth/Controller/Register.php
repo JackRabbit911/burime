937187b6e89d4d7d0a11e976c01639d8f0d9449e
alx <world.zay@gmail.com>
2026-02-24 15:55:10 +0300

    #116 Remove savepswd, add save and confirm functions

--- application/app/Api/Auth/Controller/Register.php
+++ application/app/Api/Auth/Controller/Register.php
@@ -4,31 +4,45 @@ declare(strict_types=1);
 
 namespace App\Api\Auth\Controller;
 
-use App\Api\Auth\Middleware\PasswordConfirmValidation;
-use App\Api\Auth\Service\OAuth;
-use App\Api\Auth\Service\TokenAuth;
-use App\Api\Auth\Model\ModelRecovery;
-use App\Api\Auth\Middleware\AuthMiddleware;
-use App\Api\Auth\Middleware\AuthValidation;
-use App\Api\Auth\Middleware\EmailValidation;
 use App\Api\Auth\Model\ModelUser;
 use App\Api\Auth\Service\SendEmail;
+use App\Api\Auth\Model\ModelConfirm;
+use App\Api\Auth\Middleware\RegisterValidation;
 use Az\Route\Route;
+use stdClass;
 
-#[Route(methods: 'post')]
 class Register extends ApiAuthController
 {
-    public function __construct(private ModelUser $model){}
-
-    #[PasswordConfirmValidation]
-    public function savepswd()
+    public function __construct(private ModelConfirm $model){}
+    
+    #[Route(methods: 'post')]
+    #[RegisterValidation]
+    public function save(SendEmail $mailer)
     {
         $data = $this->request->getBody()->getContents();
         $data= json_decode($data);
 
-        $hash = password_hash($data->password, PASSWORD_DEFAULT);
-        $this->model->update(['password' => $hash], $data->id);
+        $user = new stdClass;
+        $user->name = $data->name;
+        $user->email = $data->email;
+        $user->password = password_hash($data->password, PASSWORD_DEFAULT);
 
-        return true;
+        $code = $this->model->set($user);        
+        $mailer->register($this->request->getUri(), $user, $this->i18n->lang(), $code);
+
+        return $data;
+    }
+
+    public function confirm(ModelUser $model, string $code)
+    {
+        $data = $this->model->get($code);
+
+        if (!empty($data)) {
+            $user = json_decode($data->user, true);
+            $model->create($user);
+            return true;
+        }
+
+        return false;
     }
 }
