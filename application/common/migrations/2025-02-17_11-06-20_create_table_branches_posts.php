92138e0db69c82ddf27d7ef9d34dd3bbc817ce9b
alx <world.zay@gmail.com>
2026-02-28 10:54:50 +0300

    #119 Add status field

--- application/common/migrations/2025-02-17_11-06-20_create_table_branches_posts.php
+++ application/common/migrations/2025-02-17_11-06-20_create_table_branches_posts.php
@@ -8,10 +8,12 @@ class _2025_02_17_11_06_20_create_table_branches_posts
         `branch_id` int(10) unsigned NOT NULL,
         `post_id` int(10) unsigned NOT NULL,
         `weight` smallint(5) unsigned NOT NULL,
+        `status` tinyint(3) unsigned NOT NULL,
         UNIQUE KEY `branch_id_post_id` (`branch_id`,`post_id`),
         KEY `post_id` (`post_id`),
         KEY `weight` (`weight`),
         KEY `branch_id` (`branch_id`),
+        KEY `status` (`status`),
         CONSTRAINT `branches_posts_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE,
         CONSTRAINT `branches_posts_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin";
