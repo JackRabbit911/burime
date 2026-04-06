64944136f10ae52d1c0e28042460e500b297b2f9
alx <world.zay@gmail.com>
2026-04-06 17:34:29 +0300

    #139 Remove unused files

--- application/auth/messages/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'isUniqueEmail'   => 'User with this email is already registered',
-    'isRegisteredEmail' => 'This email address not found',
-    'isPairEmailPswd' => 'The email/password pair does not match',
-    'isUniqueOrOwnEmail' => 'User with this email is already registered',
-];
