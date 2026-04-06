3855da77f6df50e4d1e6145ba80c3072b0072ec5
alx <world.zay@gmail.com>
2026-04-06 14:16:56 +0300

    #139 Separete unused routes

--- application/common/config/routes/api.php
+++ application/common/config/routes/api.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use Adm\Controller\Users;
 use App\Api\Auth\Controller\Auth;
@@ -17,49 +19,43 @@ use App\Api\Message\Controller\Additional;
 use App\Api\Message\Controller\Message;
 use App\Api\Private\Controller\MyController;
 use App\Api\Private\Controller\Profile;
-// use App\Branch\Api\Controller\Branch;
-// use App\Branch\Api\Controller\BranchSave;
 use App\Burime\Controller\DeletePostApi;
 use App\Rating\RatingApi;
-use Auth\Api\Controller\O2Auth;
 use Sys\Console\Controller as ConsoleController;
+
+// use App\Branch\Api\Controller\Branch;
+// use App\Branch\Api\Controller\BranchSave;
+// use Auth\Api\Controller\O2Auth;
 
 return [
     'console'       => ['/api/console/{model}/{method}', ConsoleController::class, ['model' => '[\w\/]+']],
+    
     'rating'        => ['/api/rating/{action}/{post_id}', RatingApi::class],
     'post.confirm'  => ['/api/post/{action}/{post_id}', DeletePostApi::class],
-    // 'api.auth'      => ['/api/auth/{action}', O2Auth::class],
     'api.adm.users' => ['/api/adm/users/{id?}', Users::class],
 
-    // 'branch.help'   => ['/api/branch/help/{step}', [Branch::class, 'gethelp']],
-
-    // 'branch.save'   => ['/api/branch/save/{action?}', BranchSave::class],
-    // 'branch.delete' => ['/api/branch/delete/{id}/{draft?}', [BranchSave::class, 'delete']],
-    // 'branch.create' => ['/api/branch/create/{action}/{id?}/{draft?}', Branch::class],
-    
     'api.translate' => ['/api/gettranslate', Translate::class],
-
     'api.help'      => ['/api/my/help/{path}', Help::class, ['path' => '.*']],
     'api.reference' => ['/api/my/reference/{action}', ReferenceBooks::class],
 
     'api.authors'   => ['/api/my/common/authors/{action}/{id?}', Authors::class],
-
     'api.ba.status' => ['/api/my/branch/status/{action}/{id?}', BranchAuthorStatus::class],
     'api.branchsave'=> ['/api/my/branch/action/{action}/{id?}', BranchSave::class],
     'api.my.branch' => ['/api/my/branch/{action}/{id?}/{draft?}', MyBranch::class],
-
     'api.my.group'  => ['/api/my/group/{action}/{id?}', Group::class],
     'api.authorsave'=> ['/api/my/author/{action}/{id?}', AuthorSave::class],
-
     'api.message'   => ['/api/my/message/{action}/{id?}/{recipient?}', Message::class],
-
     'api.profile'   => ['/api/my/profile/{action?}', Profile::class],
-
     'api.additional'=> ['api/my/additional/{action}/{id}', Additional::class],
-
     'api.my'        => ['/api/my/{action}/{id?}', MyController::class],
 
     ['/api/auth/{action?}/{code?}', Auth::class],
     ['/api/register/{action}/{code?}', Register::class],
     ['/api/recovery/{action}/{id?}/{code?}', Recovery::class],
 ];
+    
+    // 'api.auth'      => ['/api/auth/{action}', O2Auth::class],
+    // 'branch.help'   => ['/api/branch/help/{step}', [Branch::class, 'gethelp']],
+    // 'branch.save'   => ['/api/branch/save/{action?}', BranchSave::class],
+    // 'branch.delete' => ['/api/branch/delete/{id}/{draft?}', [BranchSave::class, 'delete']],
+    // 'branch.create' => ['/api/branch/create/{action}/{id?}/{draft?}', Branch::class],
--- application/common/config/routes/web.php
+++ application/common/config/routes/web.php
@@ -1,16 +1,8 @@
 <?php declare(strict_types = 1);
 
-use App\Api\UserGuest;
 use App\Author\Controller\Author;
 use App\Author\Controller\Controls;
-use App\Author\Controller\Form;
-use App\Author\Controller\NoAuthor;
-use App\Branch\Controller\Branch;
-// use App\Branch\Controller\CreateSave;
-// use App\Branch\Controller\Edit;
-// use App\Branch\Controller\EditSave;
 use App\Burime\Controller\Burime;
-use App\Burime\Controller\Participation;
 use App\Burime\Controller\PostBranchSave;
 use App\Burime\Controller\PostControls;
 use App\Home\Controller\About;
@@ -19,19 +11,25 @@ use App\Home\Controller\Home;
 use App\Home\Controller\Search;
 use App\Home\Controller\StartGame;
 use App\Home\Controller\Works;
+use App\Home\Controller\AboutHowToCreate;
+use App\Rating\Rating;
+use Common\Controller\AuthFront;
+use Common\Controller\Front;
+
+use App\Api\UserGuest;
+use App\Author\Controller\Form;
+use App\Author\Controller\NoAuthor;
+use App\Burime\Controller\Participation;
 use App\Message\Controller\Message;
 use App\Private\PrivateController;
-use App\Rating\Rating;
 use App\Author\Controller\Api as AuthorApi;
 use App\Burime\Controller\Api as BurimeApi;
 use App\Chat\Controller\Chat;
-use App\Home\Controller\AboutHowToCreate;
-use Common\Controller\AuthFront;
 use Common\Controller\Avatar;
-use Common\Controller\Front;
 
 return [
-    'avatar'        => ['/avatar/{action}/{id}/{lifetime?}', Avatar::class],
+    'my'            => ['/my/{any?}', Front::class, ['any' => '.*']],
+    'auth'          => ['/auth/{any?}', AuthFront::class, ['any' => '.*']],
 
     'home'          => ['/', Home::class],
     'about.create'  => ['/about/how_to_create/{action}', AboutHowToCreate::class],
@@ -40,37 +38,22 @@ return [
     'authors'       => ['/authors', AuthorsList::class],
     'search'        => ['/search', Search::class],
     'start'         => ['/how-to-play/{action}', StartGame::class],
-
     'author.controls'=>['/author/controls/{id?}', Controls::class],
-    'author.form'   => ['/author/form/{id?}', Form::class],
-    'author.save'   => ['/author/save/{id?}', [Form::class, 'save']],
-    'author.guard'  => ['/author/need_create', NoAuthor::class],
     'author'        => ['/author/{action?}/{id}', Author::class],
-
     'branch.post'   => ['/branch/{branch_id}/save/{post_id?}', PostBranchSave::class],
     'branch'        => ['/branch/{branch_id}/{action?}/{post_id?}', Burime::class],
-
-    // 'my.branch'     => ['/my/branch/{id?}', Front::class],
-    'my'            => ['/my/{any?}', Front::class, ['any' => '.*']],
-    'auth'          => ['/auth/{any?}', AuthFront::class, ['any' => '.*']],
-
-    // 'edit'          => ['/edit/{action}/{id}', Edit::class],
-    // 'edit.save'     => ['/edit/post/{action}/{id}', EditSave::class],
-    // 'create'        => ['/create/{action}/{id?}/{draft?}', Create::class],
-    // 'create.save'   => ['/create/{action}', CreateSave::class],
-
-    'message'       => ['/message/{action}/{id?}/{author_id?}', Message::class],
-    'chat'          => ['/chat/{action?}/{room_id}/{author_id}', Chat::class],
-
-    'private'       => ['/private/{action}', PrivateController::class],
-
-    'participation' => ['/participation/{branch_id}/{action}/{author_id?}', Participation::class],
     'rating'        => ['/rating/{action}/{post_id}', Rating::class],
     'post'          => ['/post/{branch_id}/{action}/{post_id}', PostControls::class],
-    // 'branch.post'   => ['/branch/{branch_id}/save/{post_id?}', PostBranchSave::class],
-
-    'int.burime'    => ['/internal/burime/{action}/{id?}', BurimeApi::class],
-    'int.author'    => ['/internal/author/{action}/{id?}', AuthorApi::class],
-
-    'lab.react'     => ['/whoami', UserGuest::class],
+    
+    // 'avatar'        => ['/avatar/{action}/{id}/{lifetime?}', Avatar::class],
+    // 'author.form'   => ['/author/form/{id?}', Form::class],
+    // 'author.save'   => ['/author/save/{id?}', [Form::class, 'save']],
+    // 'author.guard'  => ['/author/need_create', NoAuthor::class],
+    // 'message'       => ['/message/{action}/{id?}/{author_id?}', Message::class],
+    // 'chat'          => ['/chat/{action?}/{room_id}/{author_id}', Chat::class],
+    // 'private'       => ['/private/{action}', PrivateController::class],
+    // 'participation' => ['/participation/{branch_id}/{action}/{author_id?}', Participation::class],
+    // 'int.burime'    => ['/internal/burime/{action}/{id?}', BurimeApi::class],
+    // 'int.author'    => ['/internal/author/{action}/{id?}', AuthorApi::class],
+    // 'lab.react'     => ['/whoami', UserGuest::class],
 ];
