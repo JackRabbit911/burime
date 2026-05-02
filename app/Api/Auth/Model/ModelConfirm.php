6feb4c0bf9ed398e6b82eb8b11434be6a53a2668
alx <world.zay@gmail.com>
2026-05-02 18:51:30 +0300

    #174 Add check function

--- app/Api/Auth/Model/ModelConfirm.php
+++ app/Api/Auth/Model/ModelConfirm.php
@@ -6,16 +6,17 @@ namespace App\Api\Auth\Model;
 
 use Sys\Model\MysqlModel;
 use Pecee\Pixie\Exceptions\DuplicateEntryException;
+use PDO;
 
 class ModelConfirm extends MysqlModel
 {
     public function set(object|string|null $user = null)
     {
         if (is_object($user)) {
-            $user = json_encode($user, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+            $user = json_encode($user, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
         }
 
-        while(true) {
+        while (true) {
             $code = bin2hex(random_bytes(16));
 
             try {
@@ -31,11 +32,28 @@ class ModelConfirm extends MysqlModel
         return $code;
     }
 
+    public function check(string $code, $uid)
+    {
+        $table = $this->qb->table('confirm_codes')
+            ->select('user')
+            ->where('code', '=', $code);
+
+        $json = $table->setFetchMode(PDO::FETCH_COLUMN)
+            ->first();
+
+        $user = $json ? json_decode($json) : null;
+
+        $result = $uid == $user?->id ?: false;
+        $table->delete();
+
+        return $result;
+    }
+
     public function get(string $code)
     {
         $table = $this->qb->table('confirm_codes')
             ->where('code', '=', $code);
-        
+
         $result = $table->select('code', 'user')
             ->first();
 
