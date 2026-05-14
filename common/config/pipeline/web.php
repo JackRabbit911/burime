7fd435f019981c9522842c0470005d06124e1beb
alx <world.zay@gmail.com>
2026-05-14 14:45:55 +0300

    #180 Add SessionMiddlewareto pipeline

--- common/config/pipeline/web.php
+++ common/config/pipeline/web.php
@@ -1,13 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Author\Middleware\UserAuthorsMiddleware;
-use Auth\Middleware\AuthMiddleware;
 use Auth\Middleware\OAuthMiddleware;
 use Az\Session\SessionMiddleware;
 use Sys\I18n\I18nMiddleware;
 
 $this->pipe(I18nMiddleware::class);
-// $this->pipe(SessionMiddleware::class);
+$this->pipe(SessionMiddleware::class);
 $this->pipe(OAuthMiddleware::class);
-// $this->pipe(AuthMiddleware::class);
 $this->pipe(UserAuthorsMiddleware::class);
