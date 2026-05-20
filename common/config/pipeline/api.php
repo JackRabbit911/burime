4c57ba3f838a0495ea1da1c17869ff4ae3e05e83
alx <world.zay@gmail.com>
2026-05-20 13:27:52 +0300

    #184 Apply UserAuthorsMiddleware

--- common/config/pipeline/api.php
+++ common/config/pipeline/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Api\Common\Middleware\AuthGuard;
+use App\Author\Middleware\UserAuthorsMiddleware;
 use Auth\Middleware\OAuthMiddleware;
 use Sys\Middleware\CORSMiddleware;
 use Sys\I18n\I18nMiddleware;
 
 $this->pipe(CORSMiddleware::class);
 $this->pipe(I18nMiddleware::class);
-$this->pipe(OAuthMiddleware::class, '/api/my');
+$this->pipe(OAuthMiddleware::class);
 $this->pipe(AuthGuard::class, '/api/my');
+$this->pipe(UserAuthorsMiddleware::class, '/api/informer');
