95d44d9d3367c7b490f59bda972fd0f28c7af893
alx <world.zay@gmail.com>
2026-06-03 17:31:57 +0300

    #192 Apply BooksRepo and delete unused methods

--- app/Home/Controller/Works.php
+++ app/Home/Controller/Works.php
@@ -4,56 +4,18 @@ declare(strict_types=1);
 
 namespace App\Home\Controller;
 
-use App\Api\Branch\Repository\DraftRepo;
-use App\Home\Component\BooksForm;
 use App\Home\Model\ModelWorks;
-use Common\Component\Pagination;
-use Common\Component\Switcher;
+use App\Home\Repository\BooksRepo;
+use App\Api\Branch\Repository\DraftRepo;
 use Sys\Controller\WebController;
-use Sys\Pagination\Pagination57;
 
 class Works extends WebController
 {
-    private $paginationView = 'common/component/paginator';
-    private $switcherView = 'common/component/switcher';
-    private string $title = 'Works';
-
     public function __construct(private ModelWorks $model, private DraftRepo $repo) {}
 
-    public function __invoke()
+    public function __invoke(BooksRepo $repo)
     {
-        $query_params = $this->request->getQueryParams();
-        $suffix = $query_params['show'] ?? 'cards';
-        $view = "home/books/$suffix";
-        $data = $this->makeData();
-
+        [$view, $data] = $repo->get($this->request);
         return view($view, $data);
-    }
-
-    private function makeData(?int $limit = null)
-    {
-        $query_params = $this->request->getQueryParams();
-        $page = $query_params['page'] ?? 1;
-
-        $limit = $query_params['limit'] ?? $limit ?? 24;
-        $offset = Pagination::offset((int) $page, (int) $limit);
-        $genres = $query_params['genre'] ?? [];
-        $search = $query_params['search'] ?? '';
-
-        [$books, $selected] = $this->model->get((int) $limit, $offset, $genres, $search);
-
-        $form = new BooksForm($this->request, $this->repo);
-        $pagination = new Pagination($this->request, $selected, (int) $limit);
-        $switcher = new Switcher($this->request, $this->switcherView);
-
-        return [
-            'title' => $this->title,
-            'books' => $books,
-            'switcher' => $switcher,
-            'pagination' => $pagination,
-            'form' => $form,
-            'selected' => $selected,
-            'total' => $this->model->getCount(),
-        ];
     }
 }
