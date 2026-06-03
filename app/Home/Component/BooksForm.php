19ba3a24bb826ad9dc31c0620a73932a226536ba
alx <world.zay@gmail.com>
2026-06-03 17:33:03 +0300

    #192 Update form data structure

--- app/Home/Component/BooksForm.php
+++ app/Home/Component/BooksForm.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace App\Home\Component;
 
 use App\Api\Branch\Repository\DraftRepo;
-use Psr\Http\Message\ServerRequestInterface;
 use Sys\Template\Component;
+use Psr\Http\Message\ServerRequestInterface;
 
 class BooksForm extends Component
 {
-    protected ?string $view = 'home/books/component/filter/form';
+    protected ?string $view = 'home/books/component/form';
 
-    public function __construct(private ServerRequestInterface $request, private DraftRepo $repo)
+    public function __construct(array $query_params, private DraftRepo $repo)
     {
-        $query_params = $request->getQueryParams();
-        $this->data['checked'] = $query_params['genre'] ?? [];
+        $this->data['checked'] = $query_params['filter'] ?? [];
         $this->data['search'] = $query_params['search'] ?? '';
         $this->data['show'] = $query_params['show'] ?? 'cards';
         $this->data['genres'] = $repo->getTotalGenres();
+        $this->data['reset'] = path('works') . '?show=' . $this->data['show'];
+        $this->data['placeholder'] = 'Search by title and author';
     }
 }
