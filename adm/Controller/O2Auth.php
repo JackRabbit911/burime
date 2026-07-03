c9ef6be6b085f88dc2067f8d9fbcaf5151021e27
alx <world.zay@gmail.com>
2026-07-03 15:23:21 +0300

    #203 Apply AuthRepo and add some methods

--- adm/Controller/O2Auth.php
+++ adm/Controller/O2Auth.php
@@ -6,31 +6,46 @@ namespace Adm\Controller;
 
 use App\Api\Common\Controller\ApiContractController;
 use Adm\Middleware\AuthValidation;
-use Adm\Model\ModelAuth;
-use Adm\Repository\TokensRepo;
+use Adm\Repository\AuthRepo;
 use Az\Route\Route;
-use HttpSoft\Response\JsonResponse;
+use HttpSoft\Response\EmptyResponse;
 
 class O2Auth extends ApiContractController
 {
+    public function __construct(private AuthRepo $repo) {}
+
     #[Route(methods: 'post')]
     #[AuthValidation]
-    public function login(TokensRepo $tokens)
+    public function login()
     {
-        $user = $tokens->getUser();
-        $tokens = $tokens->getTokens();
-
-        $data = [
-            'success' => true,
-            'result' => ['user' => $user]
-        ];
-
-        return new JsonResponse($data, 201, $tokens);
+        return $this->repo->login();
     }
 
     #[Route(methods: 'delete')]
     public function logout()
     {
-        return ['logout' => true];
+        $this->repo->logout($this->data['refresh']);
+        return 'Goodbye';
+    }
+
+    #[Route(methods: 'delete')]
+    public function quit()
+    {
+        $this->repo->logoutGlobal($this->data['refresh']);
+        return 'Goodbye';
+    }
+
+    #[Route(methods: 'post')]
+    public function refresh()
+    {
+        $data = $this->repo->rotate($this->data['refresh']);
+        return (($data)) ?: new EmptyResponse(401);
+    }
+
+    #[Route(methods: 'delete')]
+    public function ban()
+    {
+        $user_id = $this->data['user_id'];
+        $this->repo->ban($user_id);
     }
 }
