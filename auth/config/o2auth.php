d917763a77593e4c02b46796c730e00a0743a7cf
alx <world.zay@gmail.com>
2026-07-07 17:36:38 +0300

    #205 Union o2auth and api_o2auth configs to one

--- auth/config/o2auth.php
+++ auth/config/o2auth.php
@@ -1,14 +1,25 @@
 <?php
 
 return [
-    'name' => 'OAT',
+    'max_active_sessions' => 4,
     'algo' => 'HS256',
     'key' => env('OAUTH_KEY'),
-    'lifetime' => env('OAUTH_LIFETIME'),
+    'lifetime' => 30,
     'iss' => env('APP_NAME'),
+    'refresh_lifetime' => 300,//3600 * 2,
+    'remember_lifetime' => 3000,//3600 * 24 * 30,
+    'name' => 'OAT',
     'cookie' => [
         'path'      => '/',
-        'secure'    => false,
+        'secure'    => ENV < DEVELOPMENT,
         'httponly'  => true,
-    ]
+    ],
+    'exclude_urls' => [
+        '/api/my/stat',
+        '/api/auth/login',
+        '/api/auth/logout',
+        '/api/console',
+        '/api/rating',
+        '/api/post',
+    ],
 ];
