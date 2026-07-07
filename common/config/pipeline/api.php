691f38dd3e2565bfa80a5e5c130a6ea95044d425
alx <world.zay@gmail.com>
2026-07-07 17:38:14 +0300

    #205 Update config

--- common/config/pipeline/api.php
+++ common/config/pipeline/api.php
@@ -2,12 +2,16 @@
 
 use App\Api\Common\Middleware\AuthGuard;
 use App\Author\Middleware\UserAuthorsMiddleware;
-use Auth\Middleware\OAuthMiddleware;
+use Auth\Api\Middleware\O2AuthGuard;
+// use Auth\Middleware\OAuthMiddleware;
+use Auth\Middleware\AuthMiddleware;
 use Sys\Middleware\CORSMiddleware;
 use Sys\I18n\I18nMiddleware;
 
 $this->pipe(CORSMiddleware::class);
 $this->pipe(I18nMiddleware::class);
-$this->pipe(OAuthMiddleware::class, '/api/my');
-$this->pipe(AuthGuard::class, '/api/my');
+$this->pipe(AuthMiddleware::class);
+$this->pipe(O2AuthGuard::class, '/api/my');
+// $this->pipe(OAuthMiddleware::class, '/api/my');
+// $this->pipe(AuthGuard::class, '/api/my');
 $this->pipe(UserAuthorsMiddleware::class, '/api/informer');
