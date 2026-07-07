bef57c363483f9e1d8ad8ca34fbdcf3a79b0ec8a
alx <world.zay@gmail.com>
2026-07-07 17:34:39 +0300

    #205 Comment unused middlewares

--- common/config/pipeline/web.php
+++ common/config/pipeline/web.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-use App\Author\Middleware\UserAuthorsMiddleware;
-use Auth\Middleware\OAuthMiddleware;
-use Az\Session\SessionMiddleware;
+// use App\Author\Middleware\UserAuthorsMiddleware;
+// use Auth\Middleware\OAuthMiddleware;
+// use Az\Session\SessionMiddleware;
+use Auth\Middleware\AuthMiddleware;
 use Sys\I18n\I18nMiddleware;
 
 $this->pipe(I18nMiddleware::class);
-$this->pipe(SessionMiddleware::class);
-$this->pipe(OAuthMiddleware::class);
-$this->pipe(UserAuthorsMiddleware::class);
+$this->pipe(AuthMiddleware::class);
+// $this->pipe(SessionMiddleware::class);
+// $this->pipe(OAuthMiddleware::class);
+// $this->pipe(UserAuthorsMiddleware::class);
