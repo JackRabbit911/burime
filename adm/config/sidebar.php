459715749ab4182ff3accfb13aa5f4e55752f730
alx <world.zay@gmail.com>
2026-07-20 18:25:28 +0300

    #225 Create sidebar config file

---
+++ adm/config/sidebar.php
@@ -0,0 +1,18 @@
+<?php
+
+use Adm\Service\MenuItem;
+
+return [
+    MenuItem::create('Pages', 'pages', ADM_CONTENT),
+    MenuItem::create('Users', 'users', ADM_USERS),
+    MenuItem::create('Tests', 'testss', ADM_DEVELOP),
+    MenuItem::create('Deploy', 'deploy', ADM_DEVOPS),
+    MenuItem::create('SEO', 'seo', ADM_SEO),
+    MenuItem::create('Burime', 'burime', ADM_BURIME)
+        ->sub(
+            MenuItem::create('Works', 'works'),
+            MenuItem::create('Authors', 'authors'),
+        ),
+    MenuItem::create('SuperAdmin', 'super', ADM_ADMIN),
+    MenuItem::create('Commerce', 'commerce', ADM_COMMERCE)
+];
