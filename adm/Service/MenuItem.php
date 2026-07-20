b994536f4d2709d6e1ec95b68c3e9a1d3f085cc5
alx <world.zay@gmail.com>
2026-07-20 18:24:56 +0300

    #225 Create MenuItem class

---
+++ adm/Service/MenuItem.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adm\Service;
+
+use JsonSerializable;
+use stdClass;
+
+class MenuItem implements JsonSerializable
+{
+    private stdClass $data;
+    private int $access;
+
+    public function __construct(
+        string $label,
+        string $to,
+        ?int $access = null,
+    ) {
+        $this->data = new stdClass;
+        $this->data->label = __($label);
+        $this->data->to = $to;
+
+        if ($access) {
+            $this->access = $access;
+        }
+    }
+
+    public static function create(string $label, ?string $to = null, ?int $access = null)
+    {
+        return new self($label, $to, $access);
+    }
+
+    public function sub(...$sub)
+    {
+        $this->data->sub = $sub;
+        return $this;
+    }
+
+    public function setDisabled(int $role)
+    {
+        if (isset($this->access) && !($role & $this->access)) {
+            $this->data->disabled = true;
+        }
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->data;
+    }
+}
