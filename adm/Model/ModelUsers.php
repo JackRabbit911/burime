6893692790c17c8c8ca11a5e5664d1dd4284454b
alx <world.zay@gmail.com>
2026-07-25 12:19:45 +0300

    #229 Update get and read functions

--- adm/Model/ModelUsers.php
+++ adm/Model/ModelUsers.php
@@ -4,50 +4,56 @@ declare(strict_types=1);
 
 namespace Adm\Model;
 
+use Adm\Service\UserDTO;
 use Sys\Model\MysqlModel;
 
 class ModelUsers extends MysqlModel
 {
     private string $table = 'users';
 
-    public function get(?int $limit = null, int $offset = 0, ?string $filter = null, string $search = '')
+    public function get(?int $limit = null, int $offset = 0, ?string $filter = null, ?string $search = null)
     {
         $table = $this->qb->table($this->table)
             ->select('id', 'name', 'role')
             ->leftJoin('admins', 'admins.user_id', '=', 'id');
+
+        $result['total'] = $table->count();
 
         // if ($filter) {
         //     $table->where('name', 'LIKE', "%$filter%");
         // }
 
         if (!empty($search)) {
-           $table->where($this->qb->raw('MATCH(name) AGAINST(?)', [$search]));
+            $table->where($this->qb->raw('MATCH(name) AGAINST(? IN BOOLEAN MODE)', [$search]));
         }
-        
-        $result['total'] = $table->count();
+
+        $result['selected'] = $table->count();
 
 
         if ($limit) {
             $table->limit($limit)
                 ->offset($offset);
         }
-        
-        $result['list'] = $table->get();
+
+        $result['list'] = $table
+            ->asObject(UserDTO::class)
+            ->get();
 
         return $result;
     }
 
-    public function create(array $data){}
+    public function create(array $data) {}
 
     public function read(int $id)
     {
         return $this->qb->table($this->table)
-            ->select('id', 'name', 'dob', 'sex', 'role', 'created')
+            ->select('id', 'name', 'email', 'phone', 'dob', 'sex', 'role', 'created')
             ->leftJoin('admins', 'admins.user_id', '=', 'id')
+            ->asObject(UserDTO::class)
             ->find($id);
     }
 
-    public function update(int $id, array $data){}
+    public function update(int $id, array $data) {}
 
-    public function delete(int $id){}
+    public function delete(int $id) {}
 }
