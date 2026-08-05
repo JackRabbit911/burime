1ed1e019fcd22953e5218c2dbbdea64a1a86ea4b
alx <world.zay@gmail.com>
2026-08-05 12:06:24 +0300

    #240 Update field name

--- adm/Middleware/SearchValidation.php
+++ adm/Middleware/SearchValidation.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Adm\Middleware;
 
-use Az\Validation\Middleware\ApiValidationMiddleware;
+use App\Api\Common\Middleware\ApiContractValidation;
 
-class SearchValidation extends ApiValidationMiddleware
+class SearchValidation extends ApiContractValidation
 {
     protected function setRules($request)
     {
-        $this->validation->rule('name', 'username');
+        $this->validation->rule('search', 'username');
     }
 }
