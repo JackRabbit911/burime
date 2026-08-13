e792084c1a857fa968a79825017af11fdbe45752
alx <world.zay@gmail.com>
2026-08-13 16:01:47 +0300

    #243 Add getMode and getRoutePaths functions

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -30,22 +30,31 @@ define('DISPLAY_ERRORS', (ENV >= TESTING) ? true : false);
 
 define('IS_DEBUG', false);
 define('IS_CACHE', false);
-
 define('STRICT_MODE', false);
 
-if (PHP_SAPI === 'cli') {
-    define('MODE', 'cli');
-} elseif (strpos($_SERVER['REQUEST_URI'], '/api/') === 0) {
-    define('MODE', 'api');
-} else {
-    define('MODE', 'web');
-}
+define('MODE', getMode($_SERVER['REQUEST_URI']));
 
-define('ROUTE_PATHS', [
-    CONFIG . 'routes/' . MODE . '.php',
-    APPPATH . 'auth/config/routes.php',
-    CONFIG . 'routes/common.php',
-]);
+$GLOBALS['_MODE'] = MODE;
+
+function getMode(string $path)
+    {
+        if (PHP_SAPI === 'cli') {
+            return 'cli';
+        } elseif (strpos($path, '/api/') === 0) {
+            return 'api';
+        } else {
+            return 'web';
+        }
+    }
+
+function getRoutePaths(): array
+{
+    return [
+        CONFIG . 'routes/' . $GLOBALS['_MODE'] . '.php',
+        APPPATH . 'auth/config/routes.php',
+        CONFIG . 'routes/common.php',
+    ];
+}
 
 if (ENV > PRODUCTION) {
     ResponseHeader::addHeader('X-Robots-Tag', 'noindex, nofollow, noimageindex');
