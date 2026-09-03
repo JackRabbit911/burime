6a1f3343d4ff1691aaf89dd5eeae612930a68575
alx <world.zay@gmail.com>
2026-09-03 18:46:00 +0300

    #256 Add Develop submenu

--- adm/config/sidebar.php
+++ adm/config/sidebar.php
@@ -4,17 +4,24 @@ use Adm\Service\ADM;
 use Adm\Service\MenuItem;
 
 return [
-    MenuItem::create('Pages', 'pages', ADM::CONTENT),
-    MenuItem::create('Users', 'users', ADM::USERS),
-    MenuItem::create('Tests', 'tests', ADM::DEVELOP),
-    MenuItem::create('Deploy', 'deploy', ADM::DEVELOP),
-    MenuItem::create('SEO', 'seo', ADM::SEO),
-    MenuItem::create('Translate', 'translate', ADM::TRANSLATE),
-    MenuItem::create('Burime', 'burime', ADM::BURIME)
+    MenuItem::create('Pages', 'pages', 'book_open_text', ADM::CONTENT),
+    MenuItem::create('Users', 'users', 'users', ADM::USERS),
+    MenuItem::create('Tests', 'tests', 'bug_play', ADM::DEVELOP),
+    MenuItem::create('Develop', 'develop', 'code', ADM::DEVELOP)
         ->sub(
-            MenuItem::create('Works', 'works'),
-            MenuItem::create('Authors', 'authors'),
+            MenuItem::create('Database', 'database', 'database'),
+            MenuItem::create('Cron', 'cron', 'calendar_clock'),
+            MenuItem::create('Worker', 'worker', 'square_stack'),
+            MenuItem::create('Clean', 'clean', 'broom')
         ),
-    MenuItem::create('SuperAdmin', 'super', ADM::ADMIN),
-    MenuItem::create('Commerce', 'commerce', ADM::COMMERCE)
+    MenuItem::create('Deploy', 'eploy', 'hard_drive_download', ADM::DEVELOP),
+    MenuItem::create('SEO', 'seo', 'code_xml', ADM::SEO),
+    MenuItem::create('Translate', 'translate', 'languages', ADM::TRANSLATE),
+    MenuItem::create('Burime', 'burime', 'trophy', ADM::BURIME)
+        ->sub(
+            MenuItem::create('Works', 'works', 'book_open_text'),
+            MenuItem::create('Authors', 'authors', 'users'),
+        ),
+    MenuItem::create('SuperAdmin', 'super', 'user_star', ADM::ADMIN),
+    MenuItem::create('Commerce', 'commerce', 'dollar_sign', ADM::COMMERCE)
 ];
